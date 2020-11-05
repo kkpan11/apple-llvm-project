ffc4ae5befbf186bcc86d72abd6b2218fba72810
Michelle Casbon <michellecasbon@google.com>
2020-11-05 18:10:48 +0000

    Disable SIMD test

--- lldb/test/API/lang/swift/accelerate_simd/TestAccelerateSIMD.py
+++ lldb/test/API/lang/swift/accelerate_simd/TestAccelerateSIMD.py
@@ -1,4 +1,7 @@
 import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
-lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest])
+# SWIFT_ENABLE_TENSORFLOW
+# TODO(TF-1347): Find a better workaround in Swift for `SIMDVector`.
+# lldbinline.MakeInlineTest(__file__, globals(), decorators=[swiftTest])
+# SWIFT_ENABLE_TENSORFLOW END
