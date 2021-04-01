98490078e1d25db922f43a4ea213afe9cf0b7862
Dave Lee <davelee.com@gmail.com>
2021-04-01 13:50:59 -0700

    add comment and radar number for StackID heuristic

--- lldb/source/Target/StackID.cpp
+++ lldb/source/Target/StackID.cpp
@@ -61,10 +61,12 @@ bool lldb_private::operator<(const StackID &lhs, const StackID &rhs) {
   const lldb::addr_t lhs_cfa = lhs.GetCallFrameAddress();
   const lldb::addr_t rhs_cfa = rhs.GetCallFrameAddress();
 
-  // Check for async -> sync comparison. (stack: high, heap: low)
-  if (lhs_cfa - rhs_cfa >= 0x00007ff000000000ULL) {
+  // FIXME: rdar://76119439
+  // This heuristic is a *temporary* fallback while proper fixes are
+  // determined. The heuristic assumes the CFA of async functions is a low
+  // (heap) address, and for normal functions it's a high (stack) address.
+  if (lhs_cfa - rhs_cfa >= 0x00007ff000000000ULL)
     return true;
-  }
 
   // FIXME: We are assuming that the stacks grow downward in memory.  That's not
   // necessary, but true on
