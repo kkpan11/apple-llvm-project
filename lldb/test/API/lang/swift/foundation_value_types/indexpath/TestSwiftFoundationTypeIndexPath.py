710af6b46465e20804d5f502e4ba0463748433cf
Mishal Shah <mishal_shah@apple.com>
2021-06-08 20:56:24 -0700

    After removing overlays from Swift repo we are started seeing following failures
    * lldb-api :: lang/swift/foundation_value_types/indexpath/TestSwiftFoundationTypeIndexPath.py
    * lldb-shell :: SwiftREPL/FoundationTypes.test
    * lldb-shell :: SwiftREPL/ResilientArray.test
    * lldb-shell :: SwiftREPL/ResilientDict.test
    
    (cherry picked from commit 63c7f7b306191758b603b3290e639310bcb200eb)

--- lldb/test/API/lang/swift/foundation_value_types/indexpath/TestSwiftFoundationTypeIndexPath.py
+++ lldb/test/API/lang/swift/foundation_value_types/indexpath/TestSwiftFoundationTypeIndexPath.py
@@ -13,4 +13,4 @@ import lldbsuite.test.lldbinline as lldbinline
 from lldbsuite.test.decorators import *
 
 lldbinline.MakeInlineTest(__file__, globals(),
-                          decorators=[swiftTest,skipIf(oslist=['windows', 'linux'])])
+                          decorators=[swiftTest,skipIf(oslist=['macosx', 'windows', 'linux'])])
