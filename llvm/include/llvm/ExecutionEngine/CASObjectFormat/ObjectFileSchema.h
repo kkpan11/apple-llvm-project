kkpan11/apple-llvm-project

commit d2edec8ab1202a5174f2408cb84faedfa23b43a4

cas.o: Remove bloat from deadstrip=compile symbol table
Symbols should only be added to the list of
IndirectDeadStripCompileSymbols if they are deadstrip=compile and they
are indirectly referenced (by name). The logic in
`CompileUnitBuilder::getOrCreateTarget()` was incorrectly adding all
symbols that were indirectly referenced after being defined. Fix that,
aligning with the logic in `createSymbol()`.

For `--prefer-indirect-symbol-refs` this cuts out the number of children
from symbol tables by 20%, saves 2% total cost, when ingesting
lib/Support no-opt object files. I confirmed the number of symbols stays
the same so nothing was "lost".

Sadly I'm going to be lazy and add a FIXME for the test instead of
writing one...

llvm/lib/ExecutionEngine/CASObjectFormat/ObjectFileSchema.cpp
@@ -1852,7 +1852,12 @@ Expected<Optional<TargetRef>> CompileUnitBuilder::getOrCreateTarget(
     assert(!S.isExternal() && "External symbol has unexpected definition");
     assert(S.hasName() &&
            "Too late to create indirect reference to anonymous symbol...");
-    IndirectDeadStripCompileSymbols.push_back(*Info.Symbol);
+
+    // FIXME: Add a test confirming that this is done if and only if the
+    // deadstripping is DS_CompileUnit.
+    if (Info.Symbol->getDeadStrip() == SymbolRef::DS_CompileUnit)
+      IndirectDeadStripCompileSymbols.push_back(*Info.Symbol);
+
     Expected<Optional<NameRef>> Name = Info.Symbol->getName();
     if (!Name)
       return Name.takeError();

commit 82fc2ef53ea5dab3377bf332894c5dd928d6fc3f

cas.o: Split indirect anonymous symbols into their own table
These are rare (only happens if a dead-strippable anonymous symbol
dominates a non-anonymous symbol, which basically never happens in
practice). But future patches will be a bit simpler if they are dealt
with separately.

Note that this fixes a regression from d2edec8ab1202a51 ("cas.o: Remove
bloat from deadstrip=compile symbol table").

llvm/include/llvm/ExecutionEngine/CASObjectFormat/ObjectFileSchema.h
@@ -1051,8 +1051,9 @@ public:
   cas::CASID getDeadStripNeverID() const { return getReference(1); }
   cas::CASID getDeadStripLinkID() const { return getReference(2); }
   cas::CASID getIndirectDeadStripCompileID() const { return getReference(3); }
-  cas::CASID getStrongExternalsID() const { return getReference(4); }
-  cas::CASID getWeakExternalsID() const { return getReference(5); }
+  cas::CASID getIndirectAnonymousID() const { return getReference(4); }
+  cas::CASID getStrongExternalsID() const { return getReference(5); }
+  cas::CASID getWeakExternalsID() const { return getReference(6); }
   Expected<SymbolTableRef> getDeadStripNever() const {
     return SymbolTableRef::get(getSchema().getNode(getDeadStripNeverID()));
   }
@@ -1062,6 +1063,9 @@ public:
   Expected<SymbolTableRef> getIndirectDeadStripCompile() const {
     return SymbolTableRef::get(
         getSchema().getNode(getIndirectDeadStripCompileID()));
+  }
+  Expected<SymbolTableRef> getIndirectAnonymous() const {
+    return SymbolTableRef::get(getSchema().getNode(getIndirectAnonymousID()));
   }
   Expected<NameListRef> getStrongExternals() const {
     return NameListRef::get(getSchema().getNode(getStrongExternalsID()));
@@ -1091,7 +1095,8 @@ public:
   create(ObjectFileSchema &Schema, const Triple &TT, unsigned PointerSize,
          support::endianness Endianness, SymbolTableRef DeadStripNever,
          SymbolTableRef DeadStripLink, SymbolTableRef IndirectDeadStripCompile,
-         NameListRef StrongExternals, NameListRef WeakExternals);
+         SymbolTableRef IndirectAnonymous, NameListRef StrongExternals,
+         NameListRef WeakExternals);
 
   /// Create a compile unit out of \p G.
   ///

llvm/lib/ExecutionEngine/CASObjectFormat/ObjectFileSchema.cpp
@@ -1331,7 +1331,7 @@ CompileUnitRef::get(Expected<ObjectFormatNodeRef> Ref) {
   if (!Specific)
     return Specific.takeError();
 
-  if (Specific->getNumReferences() != 6)
+  if (Specific->getNumReferences() != 7)
     return createStringError(inconvertibleErrorCode(),
                              "corrupt compile unit refs");
 
@@ -1366,7 +1366,8 @@ Expected<CompileUnitRef> CompileUnitRef::create(
     ObjectFileSchema &Schema, const Triple &TT, unsigned PointerSize,
     support::endianness Endianness, SymbolTableRef DeadStripNever,
     SymbolTableRef DeadStripLink, SymbolTableRef IndirectDeadStripCompile,
-    NameListRef StrongSymbols, NameListRef WeakSymbols) {
+    SymbolTableRef IndirectAnonymous, NameListRef StrongSymbols,
+    NameListRef WeakSymbols) {
   Expected<Builder> B = Builder::startRootNode(Schema, KindString);
   if (!B)
     return B.takeError();
@@ -1379,7 +1380,7 @@ Expected<CompileUnitRef> CompileUnitRef::create(
 
   assert(B->IDs.size() == 1 && "Expected the root type-id?");
   B->IDs.append({DeadStripNever, DeadStripLink, IndirectDeadStripCompile,
-                 StrongSymbols, WeakSymbols});
+                 IndirectAnonymous, StrongSymbols, WeakSymbols});
   return get(B->build());
 }
 
@@ -1590,7 +1591,6 @@ public:
   ObjectFileSchema &Schema;
   raw_ostream *DebugOS;
 
-  uint64_t IndirectAnonymousSymbolCounter = 0;
   CompileUnitBuilder(ObjectFileSchema &Schema, raw_ostream *DebugOS)
       : CAS(Schema.CAS), Schema(Schema), DebugOS(DebugOS) {}
 
@@ -1604,6 +1604,7 @@ public:
   SmallVector<SymbolRef> DeadStripNeverSymbols;
   SmallVector<SymbolRef> DeadStripLinkSymbols;
   SmallVector<SymbolRef> IndirectDeadStripCompileSymbols;
+  SmallVector<SymbolRef> IndirectAnonymousSymbols;
   SmallVector<NameRef> StrongExternals;
   SmallVector<NameRef> WeakExternals;
 
@@ -1873,14 +1874,8 @@ Expected<Optional<TargetRef>> CompileUnitBuilder::getOrCreateTarget(
 
     assert(!S.isExternal());
 
-    // FIXME: Do something better than this. Instead of generating a name, this
-    // should just be an index stored directly in the indirect symbol, and the
-    // anonymous symbols that are referenced indirectly need a top-level table
-    // entry.
-    //
-    // However, currently this doesn't seem to be triggering right now, so
-    // maybe it's "fine".
-    NameStorage = ("cas.o:" + Twine(IndirectAnonymousSymbolCounter++)).str();
+    NameStorage = ("cas.o:" + Twine(IndirectAnonymousSymbols.size())).str();
+    IndirectAnonymousSymbols.push_back(*Info.Symbol);
     if (DebugOS)
       *DebugOS << "name anonymous symbol => " << *NameStorage << "\n";
     return *NameStorage;
@@ -1924,6 +1919,10 @@ Expected<CompileUnitRef> CompileUnitRef::create(ObjectFileSchema &Schema,
       SymbolTableRef::create(Schema, Builder.IndirectDeadStripCompileSymbols);
   if (!CompileTable)
     return CompileTable.takeError();
+  Expected<SymbolTableRef> AnonymousTable =
+      SymbolTableRef::create(Schema, Builder.IndirectAnonymousSymbols);
+  if (!AnonymousTable)
+    return AnonymousTable.takeError();
   Expected<NameListRef> StrongExternals =
       NameListRef::create(Schema, Builder.StrongExternals);
   if (!StrongExternals)
@@ -1933,9 +1932,10 @@ Expected<CompileUnitRef> CompileUnitRef::create(ObjectFileSchema &Schema,
   if (!WeakExternals)
     return WeakExternals.takeError();
 
-  return CompileUnitRef::create(
-      Schema, G.getTargetTriple(), G.getPointerSize(), G.getEndianness(),
-      *NeverTable, *LinkTable, *CompileTable, *StrongExternals, *WeakExternals);
+  return CompileUnitRef::create(Schema, G.getTargetTriple(), G.getPointerSize(),
+                                G.getEndianness(), *NeverTable, *LinkTable,
+                                *CompileTable, *AnonymousTable,
+                                *StrongExternals, *WeakExternals);
 }
 
 namespace {
@@ -2162,6 +2162,8 @@ Expected<std::unique_ptr<jitlink::LinkGraph>> CompileUnitRef::createLinkGraph(
   if (Error E = Builder.makeSymbols(getDeadStripLink()))
     return std::move(E);
   if (Error E = Builder.makeSymbols(getIndirectDeadStripCompile()))
+    return std::move(E);
+  if (Error E = Builder.makeSymbols(getIndirectAnonymous()))
     return std::move(E);
 
   // Return early if there were no forward declarations.