65e18d922f9836b2084da9f355c5f03fe12cafb9
Pavel Labath <pavel@labath.sk>
2021-11-25 14:01:41 +0100

    [lldb] Clarify StructuredDataImpl ownership
    StructuredDataImpl ownership semantics is unclear at best. Various
    structures were holding a non-owning pointer to it, with a comment that
    the object is owned somewhere else. From what I was able to gather that
    "somewhere else" was the SBStructuredData object, but I am not sure that
    all created object eventually made its way there. (It wouldn't matter
    even if they did, as we are leaking most of our SBStructuredData
    objects.)
    
    Since StructuredDataImpl is just a collection of two (shared) pointers,
    there's really no point in elaborate lifetime management, so this patch
    replaces all StructuredDataImpl pointers with actual objects or
    unique_ptrs to it. This makes it much easier to resolve SBStructuredData
    leaks in a follow-up patch.
    
    Differential Revision: https://reviews.llvm.org/D114791
    
    (cherry picked from commit 82de8df26f15778793dc6b1526e14779f435f2e1)

--- lldb/source/Plugins/ScriptInterpreter/Python/ScriptInterpreterPython.cpp
+++ lldb/source/Plugins/ScriptInterpreter/Python/ScriptInterpreterPython.cpp
@@ -1693,7 +1693,7 @@ StructuredData::DictionarySP ScriptInterpreterPythonImpl::OSPlugin_CreateThread(
 }
 
 StructuredData::ObjectSP ScriptInterpreterPythonImpl::CreateScriptedThreadPlan(
-    const char *class_name, StructuredDataImpl *args_data,
+    const char *class_name, const StructuredDataImpl &args_data,
     std::string &error_str, lldb::ThreadPlanSP thread_plan_sp) {
   if (class_name == nullptr || class_name[0] == '\0')
     return StructuredData::ObjectSP();
@@ -1795,7 +1795,7 @@ lldb::StateType ScriptInterpreterPythonImpl::ScriptedThreadPlanGetRunState(
 
 StructuredData::GenericSP
 ScriptInterpreterPythonImpl::CreateScriptedBreakpointResolver(
-    const char *class_name, StructuredDataImpl *args_data,
+    const char *class_name, const StructuredDataImpl &args_data,
     lldb::BreakpointSP &bkpt_sp) {
 
   if (class_name == nullptr || class_name[0] == '\0')
@@ -1865,8 +1865,8 @@ ScriptInterpreterPythonImpl::ScriptedBreakpointResolverSearchDepth(
 }
 
 StructuredData::GenericSP ScriptInterpreterPythonImpl::CreateScriptedStopHook(
-    TargetSP target_sp, const char *class_name, StructuredDataImpl *args_data,
-    Status &error) {
+    TargetSP target_sp, const char *class_name,
+    const StructuredDataImpl &args_data, Status &error) {
 
   if (!target_sp) {
     error.SetErrorString("No target for scripted stop-hook.");
@@ -2172,7 +2172,7 @@ bool ScriptInterpreterPythonImpl::BreakpointCallbackFunction(
               LLDBSwigPythonBreakpointCallbackFunction(
                   python_function_name,
                   python_interpreter->m_dictionary_name.c_str(), stop_frame_sp,
-                  bp_loc_sp, bp_option_data->m_extra_args_up.get());
+                  bp_loc_sp, bp_option_data->m_extra_args);
 
           if (!maybe_ret_val) {
 
