3f28ee98440ca1ed88a472737853e2c05bf66cc6
Augusto Noronha <augusto2112@me.com>
2021-12-02 17:12:28 -0300

    [lldb] Adapt DumpTypeDescription to take in a exe scope and implement the tsstyperef version
    (cherry picked from commit a0eab937a2bcfaa99b52cad208e29c0047f7d6d5)

--- lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
+++ lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
@@ -9032,7 +9032,8 @@ void TypeSystemClang::DumpSummary(lldb::opaque_compiler_type_t type,
 }
 
 void TypeSystemClang::DumpTypeDescription(lldb::opaque_compiler_type_t type,
-                                          lldb::DescriptionLevel level) {
+                                          lldb::DescriptionLevel level,
+                                          ExecutionContextScope *) {
   StreamFile s(stdout, false);
   DumpTypeDescription(type, &s, level);
 
@@ -9046,7 +9047,8 @@ void TypeSystemClang::DumpTypeDescription(lldb::opaque_compiler_type_t type,
 
 void TypeSystemClang::DumpTypeDescription(lldb::opaque_compiler_type_t type,
                                           Stream *s,
-                                          lldb::DescriptionLevel level) {
+                                          lldb::DescriptionLevel level,
+                                          ExecutionContextScope *) {
   if (type) {
     clang::QualType qual_type =
         RemoveWrappingTypes(GetQualType(type), {clang::Type::Typedef});
@@ -9136,7 +9138,7 @@ void TypeSystemClang::DumpTypeDescription(lldb::opaque_compiler_type_t type,
     if (buf.size() > 0) {
       s->Write(buf.data(), buf.size());
     }
-}
+  }
 }
 
 void TypeSystemClang::DumpTypeName(const CompilerType &type) {
