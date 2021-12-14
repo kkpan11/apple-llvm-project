b5e4c37c5cae285edeb66df89f9a06778510ae81
Augusto Noronha <augusto2112@me.com>
2021-12-14 17:13:16 -0300

    [lldb] Make functions that take SwiftASTContext in TSSTyperef not static
    This allows for SwiftASTContext to be initialized only when necessary
    (in TypeSystemSwiftTypeRef).
    
    (cherry picked from commit bd47e7c395fd26d7fd2868c8128bf2efa34e383a)
    (cherry picked from commit 3dc54a55f4ec19701dbd874756382e998ddf8b93)
    
     Conflicts:
    	lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntime.cpp
    	lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntime.h
    	lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntimeDynamicTypeResolution.cpp
    	lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntimeImpl.h
    x	lldb/source/Plugins/TypeSystem/Swift/TypeSystemSwiftTypeRef.cpp

--- lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntimeImpl.h
+++ lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntimeImpl.h
@@ -240,8 +240,7 @@ public:
 protected:
   /// Use the reflection context to build a TypeRef object.
   const swift::reflection::TypeRef *
-  GetTypeRef(CompilerType type, TypeSystemSwiftTypeRef *module_holder,
-             SwiftASTContext *swift_ast_context);
+  GetTypeRef(CompilerType type, TypeSystemSwiftTypeRef *module_holder);
 
   /// If \p instance points to a Swift object, retrieve its
   /// RecordTypeInfo and pass it to the callback \p fn. Repeat the
