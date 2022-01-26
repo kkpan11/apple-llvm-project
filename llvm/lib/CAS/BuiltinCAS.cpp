f5520a205cf4ce4e9434143ab0d8b252f5f8a37c
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2022-01-26 10:42:00 -0800

    CAS: Ownership simplification for internals of OnDiskHashMappedTrie
    Cribbed from https://github.com/apple/llvm-project/pull/3679

--- llvm/lib/CAS/BuiltinCAS.cpp
+++ llvm/lib/CAS/BuiltinCAS.cpp
@@ -1054,7 +1054,7 @@ Error BuiltinCAS::putCachedResult(CASID InputID, CASID OutputID) {
 Optional<BuiltinCAS::MappedContentReference>
 BuiltinCAS::openOnDisk(OnDiskHashMappedTrie &Trie, HashRef Hash) {
   if (auto Lookup = Trie.lookup(Hash))
-    return std::move(Lookup.get());
+    return Lookup.take();
   return None;
 }
 
