969938b8502ceecf55fcb895fa73addacda62b9c
Volodymyr Sapsai <vsapsai@apple.com>
2022-02-01 15:03:59 -0800

    [modules] Add a flag for TagDecl if it was a definition demoted to a declaration.
    For redeclaration chains we maintain an invariant of having only a
    single definition in the chain. In a single translation unit we make
    sure not to create duplicates. But modules are separate translation
    units and they can contain definitions for the same symbol
    independently. When we load such modules together, we need to demote
    duplicate definitions to keep the AST invariants.
    
    Some AST clients are interested in distinguishing
    declaration-that-was-demoted-from-definition and
    declaration-that-was-never-a-definition. For that purpose introducing
    `IsThisDeclarationADemotedDefinition`. No functional change intended.
    
    rdar://84677782
    
    Differential Revision: https://reviews.llvm.org/D118855
    (cherry picked from commit fa4a0f1d31e2f797c3e27eb7cad15755e46a4726)

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -4293,6 +4293,7 @@ TagDecl::TagDecl(Kind DK, TagKind TK, const ASTContext &C, DeclContext *DC,
   setEmbeddedInDeclarator(false);
   setFreeStanding(false);
   setCompleteDefinitionRequired(false);
+  TagDeclBits.IsThisDeclarationADemotedDefinition = false;
 }
 
 SourceLocation TagDecl::getOuterLocStart() const {
