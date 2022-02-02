3d63cec6c5edc788f2bd996377e9bef32f59fde9
Augusto Noronha <augusto2112@me.com>
2022-02-02 15:15:52 -0300

    [lldb] Try to map a real address to a tagged one in resolvePointer
    If we read a real process address, try to map that back as a tagged
    LLDBMLLDBMemoryReader address, so further reads originating from it
    benefit from the file-cache optimization.

--- lldb/source/Plugins/LanguageRuntime/Swift/LLDBMemoryReader.h
+++ lldb/source/Plugins/LanguageRuntime/Swift/LLDBMemoryReader.h
@@ -26,6 +26,10 @@ public:
   llvm::Optional<swift::remote::RemoteAbsolutePointer>
   resolvePointerAsSymbol(swift::remote::RemoteAddress address) override;
 
+  swift::remote::RemoteAbsolutePointer
+  resolvePointer(swift::remote::RemoteAddress address,
+                 uint64_t readValue) override;
+
   bool readBytes(swift::remote::RemoteAddress address, uint8_t *dest,
                  uint64_t size) override;
 
