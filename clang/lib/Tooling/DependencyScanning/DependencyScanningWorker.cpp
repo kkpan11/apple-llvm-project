2f71561f9660c9ceca161390512da515a5b4ffe0
Steven Wu <stevenwu@apple.com>
2022-02-15 17:12:34 -0800

    [DepScan] Fix the dependency file emission from daemon with different CWD

--- clang/lib/Tooling/DependencyScanning/DependencyScanningWorker.cpp
+++ clang/lib/Tooling/DependencyScanning/DependencyScanningWorker.cpp
@@ -480,6 +480,16 @@ void DependencyScanningWorker::computeDependenciesFromCompilerInvocation(
   // Dependencies.IncludeSystemHeaders = true;
   // Dependencies.OutputFile = "/dev/null";
 
+  // Make the output file path absolute relative to WorkingDirectory.
+  std::string &DepFile = Invocation->getDependencyOutputOpts().OutputFile;
+  if (!llvm::sys::path::is_absolute(DepFile)) {
+    // FIXME: On Windows, WorkingDirectory is insufficient for making an
+    // absolute path if OutputFile has a root name.
+    llvm::SmallString<128> Path = StringRef(DepFile);
+    llvm::sys::fs::make_absolute(WorkingDirectory, Path);
+    DepFile = Path.str().str();
+  }
+
   // FIXME: EmitDependencyFile should only be set when it's for a real
   // compilation.
   DependencyScanningAction Action(
--- clang/test/CAS/fdepscan-daemon-cwd.c
+++ clang/test/CAS/fdepscan-daemon-cwd.c
@@ -9,8 +9,12 @@
 // RUN:    -fdepscan-prefix-map=%S=/^source                               \
 // RUN:    -fdepscan-prefix-map=%t=/^build                                \
 // RUN:    -fdepscan-prefix-map-toolchain=/^toolchain                     \
-// RUN:    -fdepscan-daemon=%t/depscand -Iinclude -fsyntax-only -x c %s)
+// RUN:    -fdepscan-daemon=%t/depscand -MD -MF %t/test.d -Iinclude       \
+// RUN:    -fsyntax-only -x c %s)
 // RUN: %clang -cc1depscand -shutdown %t/depscand
+// RUN: %clang -target x86_64-apple-macos11 -MD -MF %t/test2.d            \
+// RUN:    -I %t/include -fsyntax-only -x c %s
+// RUN: diff %t/test.d %t/test2.d
 
 #include "test.h"
 
