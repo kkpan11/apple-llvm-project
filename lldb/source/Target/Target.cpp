983632d498db5fed2b9d612560dfc2853246fa05
Augusto Noronha <augusto2112@me.com>
2022-02-17 19:09:58 -0300

    [lldb] Read swift metadata from symbol rich binary
    In cases where Swift metadata is present in the symbol rich binary, read
    swift metadata from it instead of from the main binary or the process.
    This patch essentially re-uses the functionality implemented to read
    metadata from the file-cache to achieve this.

--- lldb/source/Target/Target.cpp
+++ lldb/source/Target/Target.cpp
@@ -4194,6 +4194,17 @@ bool TargetProperties::GetSwiftUseReflectionSymbols() const {
     return true;
 }
 
+bool TargetProperties::GetSwiftReadMetadataFromDSYM() const {
+  const Property *exp_property = m_collection_sp->GetPropertyAtIndex(
+      nullptr, false, ePropertyExperimental);
+  OptionValueProperties *exp_values =
+      exp_property->GetValue()->GetAsProperties();
+  if (exp_values)
+    return exp_values->GetPropertyAtIndexAsBoolean(
+        nullptr, ePropertySwiftReadMetadataFromDSYM, true);
+
+  return true;
+}
 ArchSpec TargetProperties::GetDefaultArchitecture() const {
   OptionValueArch *value = m_collection_sp->GetPropertyAtIndexAsOptionValueArch(
       nullptr, ePropertyDefaultArch);
