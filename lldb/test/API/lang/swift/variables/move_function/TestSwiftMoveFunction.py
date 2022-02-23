deec1465443cf1d0e3cc82e00c2cc7f44b62198f
Michael Gottesman <mgottesman@apple.com>
2022-02-23 14:20:32 -0800

    [move-function] Remove some unintended fr v and use more idiomatic self.assertXXX methods.
    Thanks to @kastiglione for the suggestions on the more idiomatic method usage!

--- lldb/test/API/lang/swift/variables/move_function/TestSwiftMoveFunction.py
+++ lldb/test/API/lang/swift/variables/move_function/TestSwiftMoveFunction.py
@@ -45,7 +45,7 @@ class TestSwiftMoveFunctionType(TestBase):
         self.process = self.target.LaunchSimple(None, None, os.getcwd())
         threads = lldbutil.get_threads_stopped_at_breakpoint(
             self.process, self.breakpoints[0])
-        self.assertTrue(len(threads) == 1)
+        self.assertEqual(len(threads), 1)
         self.thread = threads[0]
 
         self.do_check_copyable_value_test()
@@ -68,7 +68,7 @@ class TestSwiftMoveFunctionType(TestBase):
             pat = pattern.format(name, i+1)
             brk = self.target.BreakpointCreateBySourceRegex(
                 pat, self.main_source_spec)
-            self.assertTrue(brk.GetNumLocations() > 0, VALID_BREAKPOINT)
+            self.assertGreater(brk.GetNumLocations(), 0, VALID_BREAKPOINT)
             yield brk
 
     def do_setup_breakpoints(self):
@@ -96,15 +96,15 @@ class TestSwiftMoveFunctionType(TestBase):
         # We haven't defined varK yet.
         varK = frame.FindVariable('k')
 
-        self.assertTrue(varK.value == None, "varK initialized too early?!")
+        self.assertIsNone(varK.value, "varK initialized too early?!")
 
         # Go to break point 2. k should be valid.
         self.runCmd('continue')
-        self.assertTrue(varK.unsigned > 0, "varK not initialized?!")
+        self.assertGreater(varK.unsigned, 0, "varK not initialized?!")
 
         # Go to breakpoint 3. k should no longer be valid.
         self.runCmd('continue')
-        self.assertTrue(varK.value == None, "K is live but was moved?!")
+        self.assertIsNone(varK.value, "K is live but was moved?!")
 
         # Run so we hit the next breakpoint to jump to the next test's
         # breakpoint.
@@ -116,20 +116,20 @@ class TestSwiftMoveFunctionType(TestBase):
 
         # We haven't defined varK yet.
         varK = frame.FindVariable('k')
-        self.assertTrue(varK.value == None, "varK initialized too early?!")
+        self.assertIsNone(varK.value, "varK initialized too early?!")
 
         # Go to break point 2. k should be valid.
         self.runCmd('continue')
-        self.assertTrue(varK.unsigned > 0, "varK not initialized?!")
+        self.assertGreater(varK.unsigned, 0, "varK not initialized?!")
 
         # Go to breakpoint 3. We invalidated k
         self.runCmd('continue')
-        self.assertTrue(varK.value == None, "K is live but was moved?!")
+        self.assertIsNone(varK.value, "K is live but was moved?!")
 
         # Go to the last breakpoint and make sure that k is reinitialized
         # properly.
         self.runCmd('continue')
-        self.assertTrue(varK.unsigned > 0, "varK not initialized")
+        self.assertGreater(varK.unsigned, 0, "varK not initialized")
 
         # Run so we hit the next breakpoint to go to the next test.
         self.runCmd('continue')
@@ -147,12 +147,11 @@ class TestSwiftMoveFunctionType(TestBase):
         # move the other variable and show the correct behavior with
         # llvm.dbg.declare.
         self.runCmd('continue')
-
-        self.assertTrue(varK.unsigned > 0, "var not initialized?!")
+        self.assertGreater(varK.unsigned, 0, "var not initialized?!")
 
         # Go to breakpoint 3.
         self.runCmd('continue')
-        self.assertTrue(varK.unsigned == 0,
+        self.assertEqual(varK.unsigned, 0,
                         "dbg thinks varK is live despite move?!")
 
         # Run so we hit the next breakpoint as part of the next test.
@@ -166,16 +165,16 @@ class TestSwiftMoveFunctionType(TestBase):
 
         # Go to break point 2. k should be valid.
         self.runCmd('continue')
-        self.assertTrue(varK.unsigned > 0, "varK not initialized?!")
+        self.assertGreater(varK.unsigned, 0, "varK not initialized?!")
 
         # Go to breakpoint 3. K was invalidated.
         self.runCmd('continue')
-        self.assertTrue(varK.value == None, "K is live but was moved?!")
+        self.assertIsNone(varK.value, "K is live but was moved?!")
 
         # Go to the last breakpoint and make sure that k is reinitialized
         # properly.
         self.runCmd('continue')
-        self.assertTrue(varK.unsigned > 0, "varK not initialized")
+        self.assertGreater(varK.unsigned, 0, "varK not initialized")
 
         # Run so we hit the next breakpoint as part of the next test.
         self.runCmd('continue')
@@ -187,27 +186,26 @@ class TestSwiftMoveFunctionType(TestBase):
 
         # Check at our start point that we do not have any state for varK and
         # then continue to our next breakpoint.
-        self.assertTrue(varK.value == None, "varK should not have a value?!")
+        self.assertIsNone(varK.value, "varK should not have a value?!")
         self.runCmd('continue')
 
         # At this breakpoint, k should be defined since we are going to do
         # something with it.
-        self.assertTrue(varK.value != None, "varK should have a value?!")
+        self.assertIsNotNone(varK.value, "varK should have a value?!")
         self.runCmd('continue')
 
         # At this breakpoint, we are now in the conditional control flow part of
         # the loop. Make sure that we can see k still.
-        self.assertTrue(varK.value != None, "varK should have a value?!")
+        self.assertIsNotNone(varK.value, "varK should have a value?!")
         self.runCmd('continue')
 
         # Ok, we just performed the move. k should not be no longer initialized.
-        self.runCmd('fr v')
-        self.assertTrue(varK.value == None, "varK should not have a value?!")
+        self.assertIsNone(varK.value, "varK should not have a value?!")
         self.runCmd('continue')
 
         # Finally we left the conditional control flow part of the function. k
         # should still be None.
-        self.assertTrue(varK.value == None, "varK should not have a value!")
+        self.assertIsNone(varK.value, "varK should not have a value!")
 
     def do_check_copyable_value_ccf_false(self):
         pass
