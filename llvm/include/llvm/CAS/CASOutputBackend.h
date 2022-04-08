kkpan11/apple-llvm-project

commit a3c4bdaaf6c1f484dc8f93a84f2cc091c9e5f9d6

[clang] Fix the crash when output file is using absolute path
Fix the problem that clang -fdepscan cannot handle absolute path as
output.

rdar://86215990

llvm/include/llvm/CAS/CASOutputBackend.h
@@ -31,12 +31,14 @@ public:
   /// contents as embedded CASID. Can be \p nullptr.
   CASOutputBackend(
       std::shared_ptr<CASDB> CAS,
-      IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend);
+      IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend,
+      function_ref<std::string(StringRef)> CASPathRewriter);
   /// \param CASIDOutputBackend if set it will be used to write out the file
   /// contents as embedded CASID. Can be \p nullptr.
   CASOutputBackend(
       CASDB &CAS,
-      IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend);
+      IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend,
+      function_ref<std::string(StringRef)> CASPathRewriter);
 
 private:
   ~CASOutputBackend();
@@ -46,6 +48,7 @@ private:
   CASDB &CAS;
   std::shared_ptr<CASDB> OwnedCAS;
   IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend;
+  function_ref<std::string(StringRef)> CASPathRewriter;
 };
 
 } // namespace cas

llvm/lib/CAS/CASOutputBackend.cpp
@@ -39,16 +39,19 @@ private:
 
 CASOutputBackend::CASOutputBackend(
     std::shared_ptr<CASDB> CAS,
-    IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend)
-    : CASOutputBackend(*CAS, std::move(CASIDOutputBackend)) {
+    IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend,
+    function_ref<std::string(StringRef)> CASPathRewriter)
+    : CASOutputBackend(*CAS, std::move(CASIDOutputBackend), CASPathRewriter) {
   this->OwnedCAS = std::move(CAS);
 }
 
 CASOutputBackend::CASOutputBackend(
-    CASDB &CAS, IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend)
+    CASDB &CAS, IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend,
+    function_ref<std::string(StringRef)> CASPathRewriter)
     : StableUniqueEntityAdaptorType(
           sys::path::Style::native /*FIXME: should be posix?*/),
-      CAS(CAS), CASIDOutputBackend(std::move(CASIDOutputBackend)) {}
+      CAS(CAS), CASIDOutputBackend(std::move(CASIDOutputBackend)),
+      CASPathRewriter(CASPathRewriter) {}
 
 CASOutputBackend::~CASOutputBackend() = default;
 
@@ -98,7 +101,8 @@ CASOutputBackend::createFileImpl(StringRef ResolvedPath,
                                            Config))
             return E;
         }
-        Impl->Builder.push(ID, TreeEntry::Regular, Path);
+        std::string TreePath = CASPathRewriter(Path);
+        Impl->Builder.push(ID, TreeEntry::Regular, TreePath);
         return Error::success();
       });
 }

clang/test/CAS/fcas-fs-result-cache.c
@@ -1,0 +1,27 @@
+// RUN: rm -rf %t && mkdir -p %t
+// RUN: llvm-cas --cas %t/cas --ingest --data %s > %t/casid
+//
+// RUN: %clang -cc1 -triple x86_64-apple-macos11 -fcas builtin \
+// RUN:   -fcas-builtin-path %t/cas -fcas-fs @%t/casid -fcas-fs-result-cache \
+// RUN:   -Rcas-fs-result-cache-hit -emit-obj -o %t/output.o 2>&1 \
+// RUN:   | FileCheck %s --allow-empty --check-prefix=CACHE-MISS
+// RUN: ls %t/output.o && rm %t/output.o
+// RUN: %clang -cc1 -triple x86_64-apple-macos11 -fcas builtin \
+// RUN:   -fcas-builtin-path %t/cas -fcas-fs @%t/casid -fcas-fs-result-cache \
+// RUN:   -Rcas-fs-result-cache-hit -emit-obj -o %t/output.o 2>&1 \
+// RUN:   | FileCheck %s --check-prefix=CACHE-HIT
+// RUN: ls %t/output.o && rm %t/output.o
+// RUN: cd %t
+// RUN: %clang -cc1 -triple x86_64-apple-macos11 -fcas builtin \
+// RUN:   -fcas-builtin-path %t/cas -fcas-fs @%t/casid -fcas-fs-result-cache \
+// RUN:   -Rcas-fs-result-cache-hit -emit-obj -o output.o 2>&1 \
+// RUN:   | FileCheck %s --allow-empty --check-prefix=CACHE-MISS
+// RUN: ls %t/output.o && rm %t/output.o
+// RUN: %clang -cc1 -triple x86_64-apple-macos11 -fcas builtin \
+// RUN:   -fcas-builtin-path %t/cas -fcas-fs @%t/casid -fcas-fs-result-cache \
+// RUN:   -Rcas-fs-result-cache-hit -emit-obj -o output.o 2>&1 \
+// RUN:   | FileCheck %s --check-prefix=CACHE-HIT
+// RUN: ls %t/output.o
+//
+// CACHE-HIT: remark: result cache hit
+// CACHE-MISS-NOT: remark: result cache hit

clang/tools/driver/cc1_main.cpp
@@ -250,7 +250,16 @@ static int replayResult(llvm::cas::CASDB &CAS, llvm::cas::CASID ResultID) {
     // FIXME: This doesn't seem totally right, but it seems to "work" given
     // that the outputs are being stored at the root right now (not in a
     // working directory).
-    StringRef OutputPath = CASPath.drop_front(StringRef("/outputs/").size());
+    StringRef OutputPath = CASPath.drop_front(StringRef("/outputs").size());
+
+    // The next part of the path component is either relative/absolute,
+    // depending on if the output path is relative or absolute.
+    if (OutputPath.consume_front("/absolute"))
+      assert(llvm::sys::path::is_absolute(OutputPath) &&
+             "OutputPath should be absolute");
+    else if (OutputPath.consume_front("/relative/"))
+      assert(!llvm::sys::path::is_absolute(OutputPath) &&
+             "OutputPath should be relative");
 
     std::unique_ptr<llvm::FileOutputBuffer> Output;
     if (Expected<std::unique_ptr<llvm::FileOutputBuffer>> ExpectedOutput =
@@ -408,7 +417,17 @@ int cc1_main(ArrayRef<const char *> Argv, const char *Argv0, void *MainAddr) {
 
     // Set up the output backend so we can save / cache the result after.
     CASOutputs = llvm::makeIntrusiveRefCnt<llvm::cas::CASOutputBackend>(
-        *CAS, std::move(CASIDFileOutBackend));
+        *CAS, std::move(CASIDFileOutBackend), [](StringRef Path) {
+          // Convert the path in CAS tree depending on if the path is absolute
+          // or relative.
+          SmallString<256> NewPath;
+          if (llvm::sys::path::is_absolute(Path))
+            llvm::sys::path::append(NewPath, "absolute", Path);
+          else
+            llvm::sys::path::append(NewPath, "relative", Path);
+
+          return NewPath.str().str();
+        });
     Clang->setOutputBackend(llvm::vfs::makeMirroringOutputBackend(
         CASOutputs, std::move(NormalFileOutBackend)));
     ResultDiagsOS = std::make_unique<raw_mirroring_ostream>(

llvm/unittests/CAS/CASOutputBackendTest.cpp
@@ -35,7 +35,8 @@ TEST(CASOutputBackendTest, createFiles) {
   std::unique_ptr<CASDB> CAS = createInMemoryCAS();
   ASSERT_TRUE(CAS);
 
-  auto Outputs = makeIntrusiveRefCnt<CASOutputBackend>(*CAS, nullptr);
+  auto Outputs = makeIntrusiveRefCnt<CASOutputBackend>(
+      *CAS, nullptr, [](StringRef Path) { return Path.str(); });
 
   auto make = [&](StringRef Content, StringRef Path) {
     auto O = expectedToPointer(Outputs->createFile(Path));

commit 1d104b8714b5a14b949c5d97d82dc7bc5440c97d

CAS: Fix potential use-after-scope in CASOutputBackend
`llvm::function_ref` is never supposed to escape, but it's currently
escaping in CASOutputBackend's constructor! This was caused by
a3c4bdaaf6c1f484dc8f93a84f2cc091c9e5f9d6. Switch to unique_function.

llvm/include/llvm/CAS/CASOutputBackend.h
@@ -32,13 +32,13 @@ public:
   CASOutputBackend(
       std::shared_ptr<CASDB> CAS,
       IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend,
-      function_ref<std::string(StringRef)> CASPathRewriter);
+      unique_function<std::string(StringRef)> CASPathRewriter);
   /// \param CASIDOutputBackend if set it will be used to write out the file
   /// contents as embedded CASID. Can be \p nullptr.
   CASOutputBackend(
       CASDB &CAS,
       IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend,
-      function_ref<std::string(StringRef)> CASPathRewriter);
+      unique_function<std::string(StringRef)> CASPathRewriter);
 
 private:
   ~CASOutputBackend();
@@ -48,7 +48,7 @@ private:
   CASDB &CAS;
   std::shared_ptr<CASDB> OwnedCAS;
   IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend;
-  function_ref<std::string(StringRef)> CASPathRewriter;
+  unique_function<std::string(StringRef)> CASPathRewriter;
 };
 
 } // namespace cas

llvm/lib/CAS/CASOutputBackend.cpp
@@ -41,18 +41,19 @@ private:
 CASOutputBackend::CASOutputBackend(
     std::shared_ptr<CASDB> CAS,
     IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend,
-    function_ref<std::string(StringRef)> CASPathRewriter)
-    : CASOutputBackend(*CAS, std::move(CASIDOutputBackend), CASPathRewriter) {
+    unique_function<std::string(StringRef)> CASPathRewriter)
+    : CASOutputBackend(*CAS, std::move(CASIDOutputBackend),
+                       std::move(CASPathRewriter)) {
   this->OwnedCAS = std::move(CAS);
 }
 
 CASOutputBackend::CASOutputBackend(
     CASDB &CAS, IntrusiveRefCntPtr<llvm::vfs::OutputBackend> CASIDOutputBackend,
-    function_ref<std::string(StringRef)> CASPathRewriter)
+    unique_function<std::string(StringRef)> CASPathRewriter)
     : StableUniqueEntityAdaptorType(
           sys::path::Style::native /*FIXME: should be posix?*/),
       CAS(CAS), CASIDOutputBackend(std::move(CASIDOutputBackend)),
-      CASPathRewriter(CASPathRewriter) {}
+      CASPathRewriter(std::move(CASPathRewriter)) {}
 
 CASOutputBackend::~CASOutputBackend() = default;
 