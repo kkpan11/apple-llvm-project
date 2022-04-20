ed46dc1d8de1246b402712db1a1dc6361740cbd1
Steven Wu <stevenwu@apple.com>
2022-04-20 13:10:41 -0700

    Update cached-serialized-diagnostics test after upstream diagnostics change

--- clang/test/CAS/fcas-fs-result-cache-serialized-diagnostics.c
+++ clang/test/CAS/fcas-fs-result-cache-serialized-diagnostics.c
@@ -4,6 +4,7 @@
 // RUN: %clang -cc1 -triple x86_64-apple-macos11 -fcas-path %t/cas \
 // RUN:   -fcas-fs @%t/casid -fcache-compile-job \
 // RUN:   -Wimplicit-function-declaration \
+// RUN:   -Wno-error=implicit-function-declaration \
 // RUN:   -Rcompile-job-cache-hit -emit-obj -o %t/output.o \
 // RUN:   -serialize-diagnostic-file %t/diags %s 2>&1 \
 // RUN:   | FileCheck %s --allow-empty --check-prefix=CACHE-MISS
@@ -15,6 +16,7 @@
 // RUN: %clang -cc1 -triple x86_64-apple-macos11 -fcas-path %t/cas \
 // RUN:   -fcas-fs @%t/casid -fcache-compile-job \
 // RUN:   -Wimplicit-function-declaration \
+// RUN:   -Wno-error=implicit-function-declaration \
 // RUN:   -Rcompile-job-cache-hit -emit-obj -o %t/output.o \
 // RUN:   -serialize-diagnostic-file %t/diags %s 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=CACHE-HIT
@@ -22,9 +24,9 @@
 // RUN: c-index-test -read-diagnostics %t/diags 2>&1 | FileCheck %s --check-prefix=SERIALIZED-HIT
 
 // CACHE-HIT: remark: compile job cache hit
-// CACHE-HIT: warning: implicit declaration
+// CACHE-HIT: warning: call to undeclared function
 
-// CACHE-MISS: warning: implicit declaration
+// CACHE-MISS: warning: call to undeclared function
 // CACHE-MISS-NOT: remark: compile job cache hit
 
 // FIXME: serialized diagnostics should match the text diagnostics rdar://85234207
