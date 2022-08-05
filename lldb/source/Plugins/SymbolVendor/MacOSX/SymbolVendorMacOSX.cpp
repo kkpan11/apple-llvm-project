1a0c6d923a98f0cc51ca50fdb47e40131f6d0790
Jason Molenda <github-mail@molenda.com>
2022-08-04 19:12:58 -0700

    Remote filesystem perf improvements (#5062)
    * [NFC] Don't bother with unstripped binary w/ dSYM, don't DebugSymbols twice
    
    This patch addresses two perf issues when we find a dSYM on macOS
    after calling into the DebugSymbols framework.  First, when we have
    a local (probably stripped) binaary, we find the dSYM and we may
    be told about the location of the symbol rich binary (probably
    unstripped) which may be on a remote filesystem.  We don't need the
    unstripped binary, use the local binary we already have.
    Second, after we've found the path to the dSYM, save that in the Module
    so we don't call into DebugSymbols a second time later on to
    rediscover it.  If the user has a DBGShellCommands set, we need to
    exec that process twice, serially, which can add up.
    
    Differential Revision: https://reviews.llvm.org/D125616
    rdar://84576917
    
    (cherry picked from commit d2f3b6020fbfa2dd56ebd03c942acda961c421e2)
    
    * Defer source path remap tilde expansion until source file use
    
    When reading source path remappings out of a dSYM, lldb currently
    does tilde expansion -- expanding the tilde-username and checking
    that the destination pathname exists, for each dSYM with the path
    remappings.  This cost happens during lldb's initial process launch
    / load, an especially perf-sensitive time.  Inside Apple, we have
    dSYMs with source path remappings pointing to NFS directories where
    these extra stats for every dSYM can be very expensive if the network
    is slow.
    
    This patch instead keeps the source path mapping in the original
    tilde-username terms and does the tilde expansion when we need
    to read a specific source file from one of the modules.  We'll
    be stat'ing all of those inodes to load the source file anyway,
    so the fact that we do the tilde expansion on every source file
    we load, it doesn't cost us significantly.
    
    Differential Revision: https://reviews.llvm.org/D126435
    rdar://77091379
    
    (cherry picked from commit c274b6e5830ea88d3f55d6dc1d2b99e38cf6595e)
    (cherry picked from commit 8a467284d5608522fdf80cb78056d42d28f5ebb5)
    
    * Check that a FileSpec has a Directory component before using
    
    A follow on to my patch for https://reviews.llvm.org/D126435
    hit by an x86_64 linux bot; I assumed that a FileSpec had a
    directory component and checked if the first character was a
    '~'.  This was not a valid assumption.
    
    (cherry picked from commit bd67468645c08a96aec7839c30385956edd88021)
    
    * One further tweak for realpathing filepath to match dyld
    
    I missed one place I need to realpath the build artifact path,
    to make it match the path we get back from dyld.
    
    (cherry picked from commit ea6d0c4b2a1ec9c12aa5513ddf574f3146cf9025)

--- lldb/source/Plugins/SymbolVendor/MacOSX/SymbolVendorMacOSX.cpp
+++ lldb/source/Plugins/SymbolVendor/MacOSX/SymbolVendorMacOSX.cpp
@@ -154,6 +154,12 @@ SymbolVendorMacOSX::CreateInstance(const lldb::ModuleSP &module_sp,
           ObjectFile::FindPlugin(module_sp, &dsym_fspec, 0,
                                  FileSystem::Instance().GetByteSize(dsym_fspec),
                                  dsym_file_data_sp, dsym_file_data_offset);
+      // Important to save the dSYM FileSpec so we don't call
+      // Symbols::LocateExecutableSymbolFile a second time while trying to
+      // add the symbol ObjectFile to this Module.
+      if (dsym_objfile_sp && !module_sp->GetSymbolFileFileSpec()) {
+        module_sp->SetSymbolFileFileSpec(dsym_fspec);
+      }
       if (UUIDsMatch(module_sp.get(), dsym_objfile_sp.get(), feedback_strm)) {
         // We need a XML parser if we hope to parse a plist...
         if (XMLDocument::XMLEnabled()) {
@@ -236,13 +242,6 @@ SymbolVendorMacOSX::CreateInstance(const lldb::ModuleSP &module_sp,
                                   !original_DBGSourcePath_value.empty()) {
                                 DBGSourcePath = original_DBGSourcePath_value;
                               }
-                              if (DBGSourcePath[0] == '~') {
-                                FileSpec resolved_source_path(
-                                    DBGSourcePath.c_str());
-                                FileSystem::Instance().Resolve(
-                                    resolved_source_path);
-                                DBGSourcePath = resolved_source_path.GetPath();
-                              }
                               module_sp->GetSourceMappingList().Append(
                                   key, ConstString(DBGSourcePath), true);
                               // With version 2 of DBGSourcePathRemapping, we
@@ -275,11 +274,6 @@ SymbolVendorMacOSX::CreateInstance(const lldb::ModuleSP &module_sp,
                                            DBGBuildSourcePath);
                     plist.GetValueAsString("DBGSourcePath", DBGSourcePath);
                     if (!DBGBuildSourcePath.empty() && !DBGSourcePath.empty()) {
-                      if (DBGSourcePath[0] == '~') {
-                        FileSpec resolved_source_path(DBGSourcePath.c_str());
-                        FileSystem::Instance().Resolve(resolved_source_path);
-                        DBGSourcePath = resolved_source_path.GetPath();
-                      }
                       module_sp->GetSourceMappingList().Append(
                           ConstString(DBGBuildSourcePath),
                           ConstString(DBGSourcePath), true);
