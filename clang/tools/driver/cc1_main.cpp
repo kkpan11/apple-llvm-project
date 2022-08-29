21ac2fe5330e4ab1ce94557ee2c438418d4e790d
Ben Langmuir <blangmuir@apple.com>
2022-08-29 13:07:37 -0700

    [clang][cas] Add cc1 option -fmodule-file-cache-key=
    This option provides a way to read modules that were built using
    -fcache-compile-job by looking them up in the action cache. This will be
    used as a building block for adding compile-job caching support to
    explicit modules builds. A future change will teach the dep scanner to
    produce invocations that build modules with caching enabled, and load
    them using the action cache.
    
    The option takes a string of the form <path>=<key>, and reads a
    compile-job cache result for <key> from the action cache. The contents
    of the previously built pcm are made available as if they were at
    <path>. The path is significant for -gmodules builds, which embed the
    path for debug info. Otherwise, the path must at least be unique. The
    intention is that we will eventually canonicalize this path when
    building with prefix mappings.
    
    The -fmodule-file-cache-key= option is currently designed to be combined
    with the existing -fmodule-file= option -- the cache key makes the
    module file available, while -fmodule-file= controls when it is imported
    (eagerly or lazily).

--- clang/tools/driver/cc1_main.cpp
+++ clang/tools/driver/cc1_main.cpp
@@ -507,6 +507,8 @@ Optional<int> CompileJobCache::tryReplayCachedResult(CompilerInstance &Clang) {
 
   assert(ResultCacheKey.has_value() && "ResultCacheKey not initialized?");
 
+  Clang.setCompileJobCacheKey(*ResultCacheKey);
+
   Expected<bool> ReplayedResult =
       DisableCachedCompileJobReplay
           ? false
