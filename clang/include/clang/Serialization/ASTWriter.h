8d1c30d32c6d9237db4ccae094ee1193555a9782
Jan Svoboda <jan_svoboda@apple.com>
2022-11-01 18:45:55 -0700

    [clang][modules] NFCI: Unify FileID writing/reading
    This patch adds new functions for writing/reading `FileID`s and uses them to replace some ad-hoc code.
    
    Reviewed By: dexonsmith
    
    Differential Revision: https://reviews.llvm.org/D137211
    
    (cherry picked from commit fdbc55a51a792c65d4a1fd1f5d8e6ab19e0187d2)

--- clang/include/clang/Serialization/ASTWriter.h
+++ clang/include/clang/Serialization/ASTWriter.h
@@ -580,6 +580,9 @@ public:
   void AddAlignPackInfo(const Sema::AlignPackInfo &Info,
                         RecordDataImpl &Record);
 
+  /// Emit a FileID.
+  void AddFileID(FileID FID, RecordDataImpl &Record);
+
   /// Emit a source location.
   void AddSourceLocation(SourceLocation Loc, RecordDataImpl &Record);
 
