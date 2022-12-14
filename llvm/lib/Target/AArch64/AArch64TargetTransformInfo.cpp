Manoj-Kumar-BV/llvm-project

[AArch64] Fix cost model for `udiv` instruction when one of the operands is a uniform constant
Currently the model over estimates the cost of a udiv instruction with one constant. The correct cost for a udiv instruction is
insert_cost * extract_cost * num_elements

Reviewed By: dmgreen

Differential Revision: https://reviews.llvm.org/D135991

llvm/lib/Target/AArch64/AArch64TargetTransformInfo.cpp
@@ -2171,6 +2171,18 @@ InstructionCost AArch64TTIImpl::getArithmeticInstrCost(
           Cost *= 4;
         return Cost;
       } else {
+        // If one of the operands is a uniform constant then the cost for each
+        // element is Cost for insertion, extraction and division.
+        // Insertion cost = 2, Extraction Cost = 2, Division = cost for the
+        // operation with scalar type
+        if ((Op1Info.isConstant() && Op1Info.isUniform()) ||
+            (Op2Info.isConstant() && Op2Info.isUniform())) {
+          if (auto *VTy = dyn_cast<FixedVectorType>(Ty)) {
+            InstructionCost DivCost = BaseT::getArithmeticInstrCost(
+                Opcode, Ty->getScalarType(), CostKind, Op1Info, Op2Info);
+            return (4 + DivCost) * VTy->getNumElements();
+          }
+        }
         // On AArch64, without SVE, vector divisions are expanded
         // into scalar divisions of each pair of elements.
         Cost += getArithmeticInstrCost(Instruction::ExtractElement, Ty,

kkpan11/apple-llvm-project

(cherry-picked from 6e4cea55f0d1)

llvm/lib/Target/AArch64/AArch64TargetTransformInfo.cpp
@@ -1835,6 +1835,18 @@ InstructionCost AArch64TTIImpl::getArithmeticInstrCost(
     InstructionCost Cost = BaseT::getArithmeticInstrCost(
         Opcode, Ty, CostKind, Opd1Info, Opd2Info, Opd1PropInfo, Opd2PropInfo);
     if (Ty->isVectorTy()) {
+      // If one of the operands is a uniform constant then the cost for each
+      // element is Cost for insertion, extraction and division.
+      // Insertion cost = 2, Extraction Cost = 2, Division = cost for the
+      // operation with scalar type
+      if ( Opd1Info == TargetTransformInfo::OK_UniformConstantValue ||
+          Opd2Info == TargetTransformInfo::OK_UniformConstantValue) {
+        if (auto *VTy = dyn_cast<FixedVectorType>(Ty)) {
+          InstructionCost DivCost = BaseT::getArithmeticInstrCost(
+              Opcode, Ty->getScalarType(), CostKind, Opd1Info, Opd2Info);
+          return (4 + DivCost) * VTy->getNumElements();
+        }
+      }
       // On AArch64, vector divisions are not supported natively and are
       // expanded into scalar divisions of each pair of elements.
       Cost += getArithmeticInstrCost(Instruction::ExtractElement, Ty, CostKind,