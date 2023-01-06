1a7aee8dac381574861de6aa99c19262a312b210
Yitzhak Mandelbaum <yitzhakm@google.com>
2023-01-06 01:07:28 +0000

    Revert "[clang][dataflow] Only model struct fields that are used in the function being analyzed."
    This reverts commit 5e8f597c2fedc740b71f07dfdb1ef3c2d348b193. It caused msan and ubsan breakages.
    
    (cherry picked from commit 2b1a517a92bfdfa3b692a660e19a2bb22513a567)

--- clang/unittests/Analysis/FlowSensitive/TransferTest.cpp
+++ clang/unittests/Analysis/FlowSensitive/TransferTest.cpp
@@ -17,7 +17,6 @@
 #include "clang/Analysis/FlowSensitive/Value.h"
 #include "clang/Basic/LangStandard.h"
 #include "llvm/ADT/ArrayRef.h"
-#include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Testing/Support/Error.h"
@@ -41,22 +40,17 @@ void runDataflow(llvm::StringRef Code, Matcher Match,
                  LangStandard::Kind Std = LangStandard::lang_cxx17,
                  llvm::StringRef TargetFun = "target") {
   using ast_matchers::hasName;
-  llvm::SmallVector<std::string, 3> ASTBuildArgs = {
-      "-fsyntax-only", "-fno-delayed-template-parsing",
-      "-std=" +
-          std::string(LangStandard::getLangStandardForKind(Std).getName())};
-  auto AI =
-      AnalysisInputs<NoopAnalysis>(Code, hasName(TargetFun),
-                                   [&Options](ASTContext &C, Environment &) {
-                                     return NoopAnalysis(C, Options);
-                                   })
-          .withASTBuildArgs(ASTBuildArgs);
-  if (Options.BuiltinTransferOpts &&
-      Options.BuiltinTransferOpts->ContextSensitiveOpts)
-    AI = std::move(AI).withContextSensitivity();
   ASSERT_THAT_ERROR(
       checkDataflow<NoopAnalysis>(
-          std::move(AI),
+          AnalysisInputs<NoopAnalysis>(Code, hasName(TargetFun),
+                                       [Options](ASTContext &C, Environment &) {
+                                         return NoopAnalysis(C, Options);
+                                       })
+              .withASTBuildArgs(
+                  {"-fsyntax-only", "-fno-delayed-template-parsing",
+                   "-std=" +
+                       std::string(LangStandard::getLangStandardForKind(Std)
+                                       .getName())}),
           /*VerifyResults=*/
           [&Match](const llvm::StringMap<DataflowAnalysisState<NoopLattice>>
                        &Results,
@@ -158,7 +152,6 @@ TEST(TransferTest, StructVarDecl) {
 
     void target() {
       A Foo;
-      (void)Foo.Bar;
       // [[p]]
     }
   )";
@@ -206,7 +199,6 @@ TEST(TransferTest, StructVarDeclWithInit) {
 
     void target() {
       A Foo = Gen();
-      (void)Foo.Bar;
       // [[p]]
     }
   )";
@@ -247,13 +239,11 @@ TEST(TransferTest, StructVarDeclWithInit) {
 TEST(TransferTest, ClassVarDecl) {
   std::string Code = R"(
     class A {
-     public:
       int Bar;
     };
 
     void target() {
       A Foo;
-      (void)Foo.Bar;
       // [[p]]
     }
   )";
@@ -347,10 +337,6 @@ TEST(TransferTest, SelfReferentialReferenceVarDecl) {
 
     void target() {
       A &Foo = getA();
-      (void)Foo.Bar.FooRef;
-      (void)Foo.Bar.FooPtr;
-      (void)Foo.Bar.BazRef;
-      (void)Foo.Bar.BazPtr;
       // [[p]]
     }
   )";
@@ -493,10 +479,6 @@ TEST(TransferTest, SelfReferentialPointerVarDecl) {
 
     void target() {
       A *Foo = getA();
-      (void)Foo->Bar->FooRef;
-      (void)Foo->Bar->FooPtr;
-      (void)Foo->Bar->BazRef;
-      (void)Foo->Bar->BazPtr;
       // [[p]]
     }
   )";
@@ -910,7 +892,7 @@ TEST(TransferTest, StructParamDecl) {
     };
 
     void target(A Foo) {
-      (void)Foo.Bar;
+      (void)0;
       // [[p]]
     }
   )";
@@ -1071,9 +1053,6 @@ TEST(TransferTest, DerivedBaseMemberClass) {
       int APrivate;
     public:
       int APublic;
-
-    private:
-      friend void target();
     };
 
     class B : public A {
@@ -1082,20 +1061,10 @@ TEST(TransferTest, DerivedBaseMemberClass) {
       int BProtected;
     private:
       int BPrivate;
-
-    private:
-      friend void target();
     };
 
     void target() {
       B Foo;
-      (void)Foo.ADefault;
-      (void)Foo.AProtected;
-      (void)Foo.APrivate;
-      (void)Foo.APublic;
-      (void)Foo.BDefault;
-      (void)Foo.BProtected;
-      (void)Foo.BPrivate;
       // [[p]]
     }
   )";
@@ -1234,7 +1203,6 @@ TEST(TransferTest, DerivedBaseMemberStructDefault) {
 
     void target() {
       B Foo;
-      (void)Foo.Bar;
       // [[p]]
     }
   )";
@@ -1558,11 +1526,7 @@ TEST(TransferTest, UnionThisMember) {
       int Bar;
 
       void target() {
-        A a;
-        // Mention the fields to ensure they're included in the analysis.
-        (void)a.Foo;
-        (void)a.Bar;
-        // [[p]]
+        (void)0; // [[p]]
       }
     };
   )";
@@ -1814,7 +1778,6 @@ TEST(TransferTest, TemporaryObject) {
 
     void target() {
       A Foo = A();
-      (void)Foo.Bar;
       // [[p]]
     }
   )";
@@ -1852,7 +1815,6 @@ TEST(TransferTest, ElidableConstructor) {
 
     void target() {
       A Foo = A();
-      (void)Foo.Bar;
       // [[p]]
     }
   )";
@@ -1890,7 +1852,6 @@ TEST(TransferTest, AssignmentOperator) {
     void target() {
       A Foo;
       A Bar;
-      (void)Foo.Baz;
       // [[p1]]
       Foo = Bar;
       // [[p2]]
@@ -1953,7 +1914,6 @@ TEST(TransferTest, CopyConstructor) {
 
     void target() {
       A Foo;
-      (void)Foo.Baz;
       A Bar = Foo;
       // [[p]]
     }
@@ -1999,7 +1959,6 @@ TEST(TransferTest, CopyConstructorWithParens) {
 
     void target() {
       A Foo;
-      (void)Foo.Baz;
       A Bar((A(Foo)));
       // [[p]]
     }
@@ -2060,7 +2019,6 @@ TEST(TransferTest, MoveConstructor) {
     void target() {
       A Foo;
       A Bar;
-      (void)Foo.Baz;
       // [[p1]]
       Foo = std::move(Bar);
       // [[p2]]
