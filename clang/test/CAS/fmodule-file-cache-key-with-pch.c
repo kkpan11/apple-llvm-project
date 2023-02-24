0784cbe6bd62678a84df853959c86b0537d33113
Ben Langmuir <blangmuir@apple.com>
2023-02-24 14:04:53 -0800

    [llvm][cas] Make it an error to --ingest the CAS itself
    Ingesting the cas into itself is probably never intended, and it creates
    a strange result since the CAS is being mutated while it happens.
    Moreover, when CAS files are large it can double or more the size of the
    CAS.

--- clang/test/CAS/fmodule-file-cache-key-with-pch.c
+++ clang/test/CAS/fmodule-file-cache-key-with-pch.c
@@ -4,17 +4,17 @@
 
 // REQUIRES: ondisk_cas
 
-// RUN: rm -rf %t
+// RUN: rm -rf %t %t.cas
 // RUN: split-file %s %t
 
-// RUN: llvm-cas --cas %t/cas --ingest --data %t > %t/casid
+// RUN: llvm-cas --cas %t.cas --ingest --data %t > %t/casid
 
 // == Build B
 
 // RUN: %clang_cc1 -triple x86_64-apple-macos11 \
 // RUN:   -fmodules -fmodule-name=B -fno-implicit-modules \
 // RUN:   -emit-module %t/module.modulemap -o %t/B.pcm \
-// RUN:   -fcas-path %t/cas -fcas-fs @%t/casid \
+// RUN:   -fcas-path %t.cas -fcas-fs @%t/casid \
 // RUN:   -fcache-compile-job -Rcompile-job-cache &> %t/B.out.txt
 // RUN: cat %t/B.out.txt | FileCheck %s --check-prefix=CACHE-MISS
 // RUN: cat %t/B.out.txt | sed -E "s:^.*cache [a-z]+ for '([^']+)'.*$:\1:" > %t/B.key
@@ -28,7 +28,7 @@
 // RUN:   -fmodules -fmodule-name=A -fno-implicit-modules \
 // RUN:   @%t/B.import.rsp -fmodule-file=%t/B.pcm \
 // RUN:   -emit-module %t/module.modulemap -o %t/A.pcm \
-// RUN:   -fcas-path %t/cas -fcas-fs @%t/casid \
+// RUN:   -fcas-path %t.cas -fcas-fs @%t/casid \
 // RUN:   -fcache-compile-job -Rcompile-job-cache &> %t/A.out.txt
 // RUN: cat %t/A.out.txt | FileCheck %s --check-prefix=CACHE-MISS
 // RUN: cat %t/A.out.txt | sed -E "s:^.*cache [a-z]+ for '([^']+)'.*$:\1:" > %t/A.key
@@ -39,7 +39,7 @@
 // RUN:   -fmodules -fmodule-name=C -fno-implicit-modules \
 // RUN:   @%t/B.import.rsp -fmodule-file=%t/B.pcm \
 // RUN:   -emit-module %t/module.modulemap -o %t/C.pcm \
-// RUN:   -fcas-path %t/cas -fcas-fs @%t/casid \
+// RUN:   -fcas-path %t.cas -fcas-fs @%t/casid \
 // RUN:   -fcache-compile-job -Rcompile-job-cache &> %t/C.out.txt
 // RUN: cat %t/C.out.txt | FileCheck %s --check-prefix=CACHE-MISS
 // RUN: cat %t/C.out.txt | sed -E "s:^.*cache [a-z]+ for '([^']+)'.*$:\1:" > %t/C.key
@@ -53,14 +53,14 @@
 // RUN:   -fmodules -fno-implicit-modules \
 // RUN:   @%t/A.import.rsp -fmodule-file=%t/A.pcm \
 // RUN:   -emit-pch -x c-header %t/prefix.h -o %t/prefix.pch \
-// RUN:   -fcas-path %t/cas -fcas-fs @%t/casid \
+// RUN:   -fcas-path %t.cas -fcas-fs @%t/casid \
 // RUN:   -fcache-compile-job -Rcompile-job-cache &> %t/prefix.out.txt
 // RUN: cat %t/prefix.out.txt | FileCheck %s --check-prefix=CACHE-MISS
 
 // == Clear pcms to ensure they load from cache, and re-ingest with pch
 
 // RUN: rm %t/*.pcm
-// RUN: llvm-cas --cas %t/cas --ingest --data %t > %t/casid
+// RUN: llvm-cas --cas %t.cas --ingest --data %t > %t/casid
 // RUN: rm %t/*.pch
 
 // == Build tu
@@ -72,7 +72,7 @@
 // RUN:   -fmodules -fno-implicit-modules \
 // RUN:   @%t/C.import.rsp -fmodule-file=%t/C.pcm -include-pch %t/prefix.pch \
 // RUN:   -fsyntax-only %t/tu.c \
-// RUN:   -fcas-path %t/cas -fcas-fs @%t/casid \
+// RUN:   -fcas-path %t.cas -fcas-fs @%t/casid \
 // RUN:   -fcache-compile-job -Rcompile-job-cache &> %t/tu.out.txt
 // RUN: cat %t/tu.out.txt | FileCheck %s --check-prefix=CACHE-MISS
 
@@ -82,7 +82,7 @@
 // RUN:   -fmodules -fno-implicit-modules \
 // RUN:   @%t/C.import.rsp -fmodule-file=%t/C.pcm -include-pch %t/prefix.pch \
 // RUN:   -fsyntax-only %t/tu.c \
-// RUN:   -fcas-path %t/cas -fcas-fs @%t/casid \
+// RUN:   -fcas-path %t.cas -fcas-fs @%t/casid \
 // RUN:   -fcache-compile-job -Rcompile-job-cache &> %t/tu.out.2.txt
 // RUN: cat %t/tu.out.2.txt | FileCheck %s --check-prefix=CACHE-HIT
 
