3a3f29fadac48d87b926728400f85c519ed7a469
Steven Wu <stevenwu@apple.com>
2023-03-03 09:27:33 -0800

    Fix previous bad merge on clang-scan-deps
    rdar://106177248

--- clang/tools/clang-scan-deps/ClangScanDeps.cpp
+++ clang/tools/clang-scan-deps/ClangScanDeps.cpp
@@ -1124,7 +1124,8 @@ int main(int argc, const char **argv) {
     return {};
   };
 
-  if (Format == ScanningOutputFormat::Full)
+  if (Format == ScanningOutputFormat::Full ||
+      Format == ScanningOutputFormat::FullTree)
     FD.emplace(ModuleName.empty() ? Inputs.size() : 0);
 
   struct DepTreeResult {
