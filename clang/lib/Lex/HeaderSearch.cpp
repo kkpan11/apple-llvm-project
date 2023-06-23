0e17e27cadf6478f5cb915a9449dd91c684e74d0
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2023-06-23 15:33:39 -0700

    [clang/HeaderSearch] Make sure `loadSubdirectoryModuleMaps` doesn't cause loading of regular files
    `HeaderSearch::loadSubdirectoryModuleMaps` `stat`s all the files in a directory which causes the dependency scanning
    service to load and cache their contents. This is problematic because a file may be in the process of being generated
    and could be cached by the dep-scan service while it is still incomplete.
    
    To address this change `loadSubdirectoryModuleMaps` to ignore regular files.
    
    Differential Revision: https://reviews.llvm.org/D153670
    
    (cherry picked from commit 03a0f4b61ca50a267a405a29ff1986473a55f9d9)

--- clang/lib/Lex/HeaderSearch.cpp
+++ clang/lib/Lex/HeaderSearch.cpp
@@ -1868,6 +1868,8 @@ void HeaderSearch::loadSubdirectoryModuleMaps(DirectoryLookup &SearchDir) {
   llvm::vfs::FileSystem &FS = FileMgr.getVirtualFileSystem();
   for (llvm::vfs::directory_iterator Dir = FS.dir_begin(DirNative, EC), DirEnd;
        Dir != DirEnd && !EC; Dir.increment(EC)) {
+    if (Dir->type() == llvm::sys::fs::file_type::regular_file)
+      continue;
     bool IsFramework = llvm::sys::path::extension(Dir->path()) == ".framework";
     if (IsFramework == SearchDir.isFramework())
       loadModuleMapFile(Dir->path(), SearchDir.isSystemHeaderDirectory(),
