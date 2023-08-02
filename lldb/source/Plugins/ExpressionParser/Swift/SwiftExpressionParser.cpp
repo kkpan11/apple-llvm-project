977d20ddea17f4086396f327b059da02af2a4c84
Adrian Prantl <aprantl@apple.com>
2023-08-02 14:59:51 -0700

    Add a test for adding new functions and types in the expression evaluator (NFC)

--- lldb/source/Plugins/ExpressionParser/Swift/SwiftExpressionParser.cpp
+++ lldb/source/Plugins/ExpressionParser/Swift/SwiftExpressionParser.cpp
@@ -1835,6 +1835,7 @@ SwiftExpressionParser::Parse(DiagnosticManager &diagnostic_manager,
           new SwiftASTManipulatorBase::VariableMetadataPersistent(
               persistent_variable));
 
+      // This is only exercised by the PlaygroundsREPL tests.
       persistent_state->RegisterSwiftPersistentDecl(decl);
     }
 
