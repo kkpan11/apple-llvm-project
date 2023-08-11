517567fdf34bd466c9718df08ee21e2a5d9cb427
Cyndy Ishida <cyndy_ishida@apple.com>
2023-08-11 10:12:10 -0700

    [TextAPI] Express MH_SIM_SUPPORT in tbd files.
    This adds a new optional flag sim_support which is the same as
    MH_SIM_SUPPORT in the MachO header.
    
    Reviewed By: ributzka
    
    Differential Revision: https://reviews.llvm.org/D157433
    
    (cherry picked from commit 913f21ae5c460d6fdd73ac7663534e73f8e19044)

--- llvm/lib/TextAPI/TextStubV5.cpp
+++ llvm/lib/TextAPI/TextStubV5.cpp
@@ -546,11 +546,11 @@ Expected<PackedVersion> getPackedVersion(const Object *File, TBDKey Key) {
 Expected<TBDFlags> getFlags(const Object *File) {
   TBDFlags Flags = TBDFlags::None;
   const Array *Section = File->getArray(Keys[TBDKey::Flags]);
-  if (!Section)
+  if (!Section || Section->empty())
     return Flags;
 
   for (auto &Val : *Section) {
-    // TODO: Just take first for now.
+    // FIXME: Flags currently apply to all target triples.
     const auto *Obj = Val.getAsObject();
     if (!Obj)
       return make_error<JSONStubError>(getParseErrorMsg(TBDKey::Flags));
@@ -562,6 +562,7 @@ Expected<TBDFlags> getFlags(const Object *File) {
                   .Case("flat_namespace", TBDFlags::FlatNamespace)
                   .Case("not_app_extension_safe",
                         TBDFlags::NotApplicationExtensionSafe)
+                  .Case("sim_support", TBDFlags::SimulatorSupport)
                   .Default(TBDFlags::None);
           Flags |= TBDFlag;
         });
@@ -652,6 +653,7 @@ Expected<IFPtr> parseToInterfaceFile(const Object *File) {
   F->setTwoLevelNamespace(!(Flags & TBDFlags::FlatNamespace));
   F->setApplicationExtensionSafe(
       !(Flags & TBDFlags::NotApplicationExtensionSafe));
+  F->setSimulatorSupport((Flags & TBDFlags::SimulatorSupport));
   for (auto &T : Targets)
     F->addTarget(T);
   for (auto &[Lib, Targets] : Clients)
@@ -917,6 +919,8 @@ Array serializeFlags(const InterfaceFile *File) {
     Flags.emplace_back("flat_namespace");
   if (!File->isApplicationExtensionSafe())
     Flags.emplace_back("not_app_extension_safe");
+  if (File->hasSimulatorSupport())
+    Flags.emplace_back("sim_support");
   return serializeScalar(TBDKey::Attributes, std::move(Flags));
 }
 
