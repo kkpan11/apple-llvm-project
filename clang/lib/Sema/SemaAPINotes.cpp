ea634acb62011012e1d38e19a137a59d8fe1f958
Egor Zhdan <e_zhdan@apple.com>
2023-08-21 15:29:45 +0100

    [APINotes] Do not require spelling out inline namespaces
    rdar://113403829

--- clang/lib/Sema/SemaAPINotes.cpp
+++ clang/lib/Sema/SemaAPINotes.cpp
@@ -824,7 +824,8 @@ void Sema::ProcessAPINotes(Decl *D) {
         {
           auto CurrentNamespace = NamespaceContext;
           while (CurrentNamespace) {
-            NamespaceStack.push(CurrentNamespace);
+            if (!CurrentNamespace->isInlineNamespace())
+              NamespaceStack.push(CurrentNamespace);
             CurrentNamespace =
                 dyn_cast<NamespaceDecl>(CurrentNamespace->getParent());
           }
