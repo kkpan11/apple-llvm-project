0o001/llvm-project

[APINotes] Introduce context kind for C++ namespaces
Previously, `clang::api_notes::ContextID` used to refer to either ObjC class or an ObjC protocol.

Since we're going to support C++ namespaces, we need a way to represent a C++ namespace as a context.

This change introduces `clang::api_notes::ContextKind` which disambiguates between the three possible context kinds.

rdar://113403829

clang/include/clang/APINotes/APINotesReader.h
@@ -204,6 +204,13 @@ public:
   ///
   /// \returns information about the typedef, if known.
   VersionedInfo<TypedefInfo> lookupTypedef(llvm::StringRef name);
+
+  /// Look for the context ID of the given C++ namespace.
+  ///
+  /// \param name The name of the class we're looking for.
+  ///
+  /// \returns The ID, if known.
+  std::optional<ContextID> lookupNamespaceID(llvm::StringRef name);
 };
 
 } // end namespace api_notes

kkpan11/apple-llvm-project

clang/include/clang/APINotes/APINotesReader.h
@@ -205,6 +205,13 @@ public:
   ///
   /// \returns information about the typedef, if known.
   VersionedInfo<TypedefInfo> lookupTypedef(llvm::StringRef name);
+
+  /// Look for the context ID of the given C++ namespace.
+  ///
+  /// \param name The name of the class we're looking for.
+  ///
+  /// \returns The ID, if known.
+  llvm::Optional<ContextID> lookupNamespaceID(llvm::StringRef name);
 };
 
 } // end namespace api_notes