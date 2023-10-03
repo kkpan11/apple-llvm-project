4890cb0f6f9f02460ee558f0ed04f2fd02c9591e
Jonas Devlieghere <jonas@devlieghere.com>
2023-10-03 10:34:00 -0700

    [lldb] Expose Platform::Attach through the SB API (#68050)
    Expose Platform::Attach through the SB API.
    
    rdar://116188959
    (cherry picked from commit 2da99a11196246ab5f9787117f01b2251480607a)

--- lldb/include/lldb/API/SBDebugger.h
+++ lldb/include/lldb/API/SBDebugger.h
@@ -483,6 +483,7 @@ private:
   friend class SBProcess;
   friend class SBSourceManager;
   friend class SBStructuredData;
+  friend class SBPlatform;
   friend class SBTarget;
   friend class SBTrace;
 
