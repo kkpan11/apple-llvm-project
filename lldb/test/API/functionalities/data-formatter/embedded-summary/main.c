d796de5c467f7537ea8ecd41cac21e3643c784a0
Dave Lee <davelee.com@gmail.com>
2024-02-04 13:24:26 -0800

    Fix lawyer -> layer typo

--- lldb/test/API/functionalities/data-formatter/embedded-summary/main.c
+++ lldb/test/API/functionalities/data-formatter/embedded-summary/main.c
@@ -20,12 +20,12 @@ struct Layer {
 
 // Near copy of the record for `Player`, using a regex type name (`^Layer`).
 __attribute__((used, section("__DATA_CONST,__lldbsummaries"))) unsigned char
-    _Lawyer_type_summary[] = "\x01"     // version
-                             "\x25"     // record size
-                             "\x07"     // type name size
-                             "^Layer\0" // type name
-                             "\x1c"     // summary string size
-                             "${var.name} (${var.number})"; // summary string
+    _Layer_type_summary[] = "\x01"     // version
+                            "\x25"     // record size
+                            "\x07"     // type name size
+                            "^Layer\0" // type name
+                            "\x1c"     // summary string size
+                            "${var.name} (${var.number})"; // summary string
 
 int main() {
   struct Player player;
