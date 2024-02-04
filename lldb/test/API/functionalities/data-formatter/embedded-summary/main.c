9b0474c0a790ab83f0be24d3d521cfb79d2031ca
Dave Lee <davelee.com@gmail.com>
2024-02-03 21:06:26 -0800

    [lldb] Fix embedded type summary regex handling

--- lldb/test/API/functionalities/data-formatter/embedded-summary/main.c
+++ lldb/test/API/functionalities/data-formatter/embedded-summary/main.c
@@ -13,10 +13,27 @@ __attribute__((used, section("__DATA_CONST,__lldbsummaries"))) unsigned char
                              "\x1c"     // summary string size
                              "${var.name} (${var.number})"; // summary string
 
+struct Layer {
+  char *name;
+  int number;
+};
+
+// Near copy of the record for `Player`, using a regex type name (`^Layer`).
+__attribute__((used, section("__DATA_CONST,__lldbsummaries"))) unsigned char
+    _Lawyer_type_summary[] = "\x01"     // version
+                             "\x25"     // record size
+                             "\x07"     // type name size
+                             "^Layer\0" // type name
+                             "\x1c"     // summary string size
+                             "${var.name} (${var.number})"; // summary string
+
 int main() {
   struct Player player;
   player.name = "Dirk";
   player.number = 41;
+  struct Layer layer;
+  layer.name = "crust";
+  layer.number = 3;
   puts("break here");
   return 0;
 }
