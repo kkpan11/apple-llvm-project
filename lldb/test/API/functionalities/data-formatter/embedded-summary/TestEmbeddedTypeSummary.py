1606ff234d50aca79a8383fbd2a8792ce5d396bf
Dave Lee <davelee.com@gmail.com>
2024-02-05 14:41:24 -0800

    log lldb formatters in test

--- lldb/test/API/functionalities/data-formatter/embedded-summary/TestEmbeddedTypeSummary.py
+++ lldb/test/API/functionalities/data-formatter/embedded-summary/TestEmbeddedTypeSummary.py
@@ -7,6 +7,7 @@ from lldbsuite.test import lldbutil
 class TestCase(TestBase):
     @skipUnlessDarwin
     def test(self):
+        self.runCmd("log enable lldb formatters")
         self.build()
         lldbutil.run_to_source_breakpoint(self, "break here", lldb.SBFileSpec("main.c"))
         self.expect("v player", substrs=['"Dirk" (41)'])
