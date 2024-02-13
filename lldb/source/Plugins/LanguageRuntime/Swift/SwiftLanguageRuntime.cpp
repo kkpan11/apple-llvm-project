da3753f79ccfa9adb2e7271e06cd40cb76105d49
Adrian Prantl <aprantl@apple.com>
2024-02-13 10:57:26 -0800

    Invalidate the reflection metadata cache when new symbols are added.
    Reflection context maintains several caches for TypeInfos and TypeRefs
    that also include types it got from LLDBTypeInforProvider, which reads
    types from DWARF. If new DWARF is available we need to invalidate
    these caches.
    
    rdar://122432501

--- lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntime.cpp
+++ lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntime.cpp
@@ -277,6 +277,7 @@ public:
     return {};
   }
 
+  void SymbolsDidLoad(const ModuleList &module_list) {}
   void ModulesDidLoad(const ModuleList &module_list) {}
 
   bool IsStoredInlineInBuffer(CompilerType type) {
@@ -2357,6 +2358,10 @@ SwiftLanguageRuntime::GetReflectionContext() {
   if (m_impl)
     return m_impl->GetReflectionContext();
   return m_stub->GetReflectionContext();
+}
+
+void SwiftLanguageRuntime::SymbolsDidLoad(const ModuleList &module_list) {
+  FORWARD(SymbolsDidLoad, module_list);
 }
 
 bool SwiftLanguageRuntime::GetDynamicTypeAndAddress(
