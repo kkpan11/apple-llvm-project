729fa59f8e48565dd633a6c126de305543898d84
Jan Svoboda <jan_svoboda@apple.com>
2024-02-22 08:47:47 -0800

    [clang][include-tree] Fix spurious dependencies

--- clang/include/clang/Basic/Module.h
+++ clang/include/clang/Basic/Module.h
@@ -350,6 +350,10 @@ public:
   LLVM_PREFERRED_TYPE(bool)
   unsigned IsInferred : 1;
 
+  /// Whether this is an inferred submodule that's missing from the umbrella
+  /// header.
+  unsigned IsInferredMissingFromUmbrellaHeader : 1;
+
   /// Whether we should infer submodules for this module based on
   /// the headers.
   ///
