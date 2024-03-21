7c170520e42bd74fa3c5aaae3ac80491757443bd
Jonas Devlieghere <jonas@devlieghere.com>
2024-03-20 19:43:44 -0700

    [lldb] Document the purpose of Resources/Swift in LLDBFramework.cmake

--- lldb/cmake/modules/LLDBFramework.cmake
+++ lldb/cmake/modules/LLDBFramework.cmake
@@ -135,6 +135,10 @@ if(NOT APPLE_EMBEDDED)
   )
 
   if(LLDB_FRAMEWORK_COPY_SWIFT_RESOURCES)
+    # To run the LLDB test suite, we need to be able to find some of the Swift
+    # compiler resources. When part of a toolchain, LLDB knows where to find
+    # those. For at-desk builds, it copies over the resources from the Swift
+    # build directory into LLDB.framework.
     set(RESOURCE_DIRS "clang;embedded;macosx;shims;swiftToCxx")
     foreach(DIR ${RESOURCE_DIRS})
       add_custom_command(TARGET liblldb POST_BUILD
