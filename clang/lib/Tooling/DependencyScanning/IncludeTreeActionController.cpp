260c92ca9c917cf36d9a37111f49ed35ab4cec2f
Steven Wu <stevenwu@apple.com>
2024-03-28 11:10:11 -0700

    [IncludeTree] Suppot `export_as` field in ModuleMap
    Add support for `export_as` in module map for include-tree.
    
    rdar://125421391
    (cherry picked from commit 68bc27054ff8b62dda88db20d0bb49ba46eb447d)

--- clang/lib/Tooling/DependencyScanning/IncludeTreeActionController.cpp
+++ clang/lib/Tooling/DependencyScanning/IncludeTreeActionController.cpp
@@ -541,6 +541,7 @@ getIncludeTreeModule(cas::ObjectStore &DB, Module *M) {
   Flags.InferSubmodules = M->InferSubmodules;
   Flags.InferExplicitSubmodules = M->InferExplicitSubmodules;
   Flags.InferExportWildcard = M->InferExportWildcard;
+  Flags.UseExportAsModuleLinkName = M->UseExportAsModuleLinkName;
 
   bool GlobalWildcardExport = false;
   SmallVector<ITModule::ExportList::Export> Exports;
@@ -574,8 +575,8 @@ getIncludeTreeModule(cas::ObjectStore &DB, Module *M) {
     LinkLibraries = LL->getRef();
   }
 
-  return ITModule::create(DB, M->Name, Flags, Submodules, ExportList,
-                          LinkLibraries);
+  return ITModule::create(DB, M->Name, M->ExportAsModule, Flags, Submodules,
+                          ExportList, LinkLibraries);
 }
 
 Expected<cas::IncludeTreeRoot>
