18eed9d2d51f25c42d80158b401302e24e33fa6d
Dave Lee <davelee.com@gmail.com>
2024-07-08 13:53:51 -0700

    assertTrue(False) to ensure fail

--- lldb/test/API/lang/swift/module_import/TestSwiftModuleImport.py
+++ lldb/test/API/lang/swift/module_import/TestSwiftModuleImport.py
@@ -20,14 +20,12 @@ class TestSwiftModuleImport(lldbtest.TestBase):
         log = self.getBuildArtifact("types.log")
         self.runCmd('log enable lldb types -f "%s"' % log)
         self.runCmd("expression -- 0", check=False)
-        did_fail = False
         with open(log) as f:
             pat = re.compile("-linux|target|triple|x86_64-")
             for line in f.readlines():
                 if pat.match(line):
                     print(line)
                     print(line, file=sys.stderr)
-                    did_fail = True
-        self.assertFalse(did_fail)
+        self.assertTrue(False)
         self.filecheck('platform shell cat "%s"' % log, __file__)
 #       CHECK: SwiftASTContextForExpressions{{.*}}Module import remark: loaded module 'a'
