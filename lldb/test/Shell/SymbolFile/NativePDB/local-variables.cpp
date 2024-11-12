af014c94c4a900952c7a697adf56e092ffe81b98
Vladislav Dzhidzhoev <vdzhidzhoev@accesssoftek.com>
2024-11-12 18:03:24 +0100

    [lldb][test] Fix remote Shell tests failures on Windows host (#115716)
    Since the remote Shell test execution feature was added, these tests
    should now be disabled on Windows target instead of Windows host.
    
    It should fix failures on
    https://lab.llvm.org/staging/#/builders/197/builds/76.

--- lldb/test/Shell/SymbolFile/NativePDB/local-variables.cpp
+++ lldb/test/Shell/SymbolFile/NativePDB/local-variables.cpp
@@ -1,6 +1,6 @@
 // clang-format off
 
-// REQUIRES: system-windows
+// REQUIRES: target-windows
 // RUN: %build -o %t.exe -- %s
 // RUN: env LLDB_USE_NATIVE_PDB_READER=1 %lldb -f %t.exe -s \
 // RUN:     %p/Inputs/local-variables.lldbinit 2>&1 | FileCheck %s
