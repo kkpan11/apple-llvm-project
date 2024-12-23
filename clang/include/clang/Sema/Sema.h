e465c257ac16e3a771158bf77d0dc93489f6c5bc
Gabor Horvath <gaborh@apple.com>
2024-12-23 15:23:32 +0000

    [Clang] Permit noescape on non-pointer types
    In modern C++ we often use span, string_view or other view objects instead of
    raw pointers. This PR opens the door to mark those noescape. This can be useful
    to document the API contracts, for interop with memory safe languages like
    Swift or Rust, and possibly in the future to implement take this into account
    in codegen.
    
    The PR also adds a feature test macro. The goal is to help avoiding warnings
    when the code is compiler by earlier versions of clang that does not permit
    this attribute on non-pointer types.
    
    rdar://140196481

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -4663,12 +4663,6 @@ public:
                                       StringRef &Str,
                                       SourceLocation *ArgLocation = nullptr);
 
-  /// Determine if type T is a valid subject for a nonnull and similar
-  /// attributes. By default, we look through references (the behavior used by
-  /// nonnull), but if the second parameter is true, then we treat a reference
-  /// type as valid.
-  bool isValidPointerAttrType(QualType T, bool RefOkay = false);
-
   /// AddAssumeAlignedAttr - Adds an assume_aligned attribute to a particular
   /// declaration.
   void AddAssumeAlignedAttr(Decl *D, const AttributeCommonInfo &CI, Expr *E,
