llvm/llvm-project

[clang][cas] Avoid calling freezeConfig for every cache key
With the libclang caching APIs, it is possible to configure CASOptions
separately from the CAS used for scanning. In this setup, the CAS is not
pre-cached in the CASOptions and ends up being re-opened for every cache
key, which is expensive with some plugin CAS. Instead, get the hash
identifier from the already open CAS and only freezeConfig when
canonicalizing for an actual compilation.

rdar://141555438

clang/lib/Frontend/CompileJobCacheKey.cpp
@@ -207,7 +207,10 @@ canonicalizeForCaching(llvm::cas::ObjectStore &CAS, DiagnosticsEngine &Diags,
   //
   // TODO: Extract CASOptions.Path first if we need it later since it'll
   // disappear here.
-  Invocation.getCASOpts().freezeConfig(Diags);
+  Invocation.getCASOpts() = {};
+  // Set the CASPath to the hash schema to match CASOptions::freezeConfig.
+  Invocation.getCASOpts().CASPath =
+      CAS.getContext().getHashSchemaIdentifier().str();
 
   // TODO: Canonicalize DiagnosticOptions here to be "serialized" only. Pass in
   // a hook to mirror diagnostics to stderr (when writing there), and handle
@@ -223,47 +226,51 @@ clang::canonicalizeAndCreateCacheKeys(ObjectStore &CAS,
                                       DiagnosticsEngine &Diags,
                                       CompilerInvocation &CI,
                                       CompileJobCachingOptions &Opts) {
-  if (!CI.getFrontendOpts().CASInputFileCacheKey.empty()) {
-    Opts = canonicalizeForCaching(CAS, Diags, CI);
-    auto CacheKey = createCompileJobCacheKeyImpl(CAS, Diags, CI);
-    if (!CacheKey)
-      return std::nullopt;
-
-    auto ID = CAS.parseID(CI.getFrontendOpts().CASInputFileCacheKey);
-    if (!ID) {
-      Diags.Report(diag::err_cas_cannot_parse_input_cache_key)
-          << CI.getFrontendOpts().CASInputFileCacheKey;
-      return std::nullopt;
-    }
-    auto Value = Cache.get(*ID);
-    if (!Value) {
-      Diags.Report(diag::err_cas_cannot_lookup_input_cache_key)
-          << Value.takeError();
-      return std::nullopt;
-    }
-    if (!*Value)  {
-      Diags.Report(diag::err_cas_missing_input_cache_entry)
-          << llvm::cas::ObjectStore::createUnknownObjectError(*ID);
-      return std::nullopt;
-    }
-
-    CI.getFrontendOpts().CASInputFileCASID = Value.get()->toString();
-    CI.getFrontendOpts().CASInputFileCacheKey.clear();
-
-    CompilerInvocation CICopy = CI;
-    (void)canonicalizeForCaching(CAS, Diags, CICopy);
-    auto CanonicalCacheKey = createCompileJobCacheKeyImpl(CAS, Diags, CICopy);
-    if (!CanonicalCacheKey)
-      return std::nullopt;
-
-    return std::make_pair(*CacheKey, *CanonicalCacheKey);
-  }
+  // Preserve and freeze CASOptions so that we do not modify behaviour of
+  // Invocation.getCASOpts().getOrCreateDatabases().
+  CASOptions CASOpts(CI.getCASOpts());
+  CASOpts.freezeConfig(Diags);
 
   Opts = canonicalizeForCaching(CAS, Diags, CI);
   auto CacheKey = createCompileJobCacheKeyImpl(CAS, Diags, CI);
   if (!CacheKey)
     return std::nullopt;
-  return std::make_pair(*CacheKey, *CacheKey);
+
+  assert(CI.getCASOpts().CASPath == CASOpts.CASPath &&
+         "cas instance has incompatible hash with cas options");
+  CI.getCASOpts() = std::move(CASOpts);
+
+  if (CI.getFrontendOpts().CASInputFileCacheKey.empty())
+    return std::make_pair(*CacheKey, *CacheKey);
+
+  auto ID = CAS.parseID(CI.getFrontendOpts().CASInputFileCacheKey);
+  if (!ID) {
+    Diags.Report(diag::err_cas_cannot_parse_input_cache_key)
+        << CI.getFrontendOpts().CASInputFileCacheKey;
+    return std::nullopt;
+  }
+  auto Value = Cache.get(*ID);
+  if (!Value) {
+    Diags.Report(diag::err_cas_cannot_lookup_input_cache_key)
+        << Value.takeError();
+    return std::nullopt;
+  }
+  if (!*Value)  {
+    Diags.Report(diag::err_cas_missing_input_cache_entry)
+        << llvm::cas::ObjectStore::createUnknownObjectError(*ID);
+    return std::nullopt;
+  }
+
+  CI.getFrontendOpts().CASInputFileCASID = Value.get()->toString();
+  CI.getFrontendOpts().CASInputFileCacheKey.clear();
+
+  CompilerInvocation CICopy = CI;
+  (void)canonicalizeForCaching(CAS, Diags, CICopy);
+  auto CanonicalCacheKey = createCompileJobCacheKeyImpl(CAS, Diags, CICopy);
+  if (!CanonicalCacheKey)
+    return std::nullopt;
+
+  return std::make_pair(*CacheKey, *CanonicalCacheKey);
 }
 
 std::optional<llvm::cas::CASID>

kkpan11/apple-llvm-project

(cherry picked from commit 3c460cc4e43df4e43af42fbbd9e00506dc7029c5)

clang/lib/Frontend/CompileJobCacheKey.cpp
@@ -181,7 +181,10 @@ canonicalizeForCaching(llvm::cas::ObjectStore &CAS, DiagnosticsEngine &Diags,
   //
   // TODO: Extract CASOptions.Path first if we need it later since it'll
   // disappear here.
-  Invocation.getCASOpts().freezeConfig(Diags);
+  Invocation.getCASOpts() = {};
+  // Set the CASPath to the hash schema to match CASOptions::freezeConfig.
+  Invocation.getCASOpts().CASPath =
+      CAS.getContext().getHashSchemaIdentifier().str();
 
   // TODO: Canonicalize DiagnosticOptions here to be "serialized" only. Pass in
   // a hook to mirror diagnostics to stderr (when writing there), and handle
@@ -194,8 +197,20 @@ canonicalizeForCaching(llvm::cas::ObjectStore &CAS, DiagnosticsEngine &Diags,
 std::optional<llvm::cas::CASID> clang::canonicalizeAndCreateCacheKey(
     llvm::cas::ObjectStore &CAS, DiagnosticsEngine &Diags,
     CompilerInvocation &Invocation, CompileJobCachingOptions &Opts) {
+  // Preserve and freeze CASOptions so that we do not modify behaviour of
+  // Invocation.getCASOpts().getOrCreateDatabases().
+  CASOptions CASOpts(Invocation.getCASOpts());
+  CASOpts.freezeConfig(Diags);
+
   Opts = canonicalizeForCaching(CAS, Diags, Invocation);
-  return createCompileJobCacheKeyImpl(CAS, Diags, Invocation);
+  auto CacheKey = createCompileJobCacheKeyImpl(CAS, Diags, Invocation);
+  if (!CacheKey)
+    return std::nullopt;
+
+  assert(Invocation.getCASOpts().CASPath == CASOpts.CASPath &&
+         "cas instance has incompatible hash with cas options");
+  Invocation.getCASOpts() = std::move(CASOpts);
+  return CacheKey;
 }
 
 std::optional<llvm::cas::CASID>