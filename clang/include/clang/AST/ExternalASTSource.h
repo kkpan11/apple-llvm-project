5792c4cdb0a6bf4b37f1e7096381ebd4a714e953
Michael Buch <michaelbuch12@gmail.com>
2025-02-07 13:18:56 +0000

    [lldb] Remove redecl-completion
    (cherry picked from commit 0a90a3658247478787d4da1b4599a965d45fec64)

--- clang/include/clang/AST/ExternalASTSource.h
+++ clang/include/clang/AST/ExternalASTSource.h
@@ -152,11 +152,6 @@ public:
   virtual bool
   FindExternalVisibleDeclsByName(const DeclContext *DC, DeclarationName Name);
 
-  virtual bool FindExternalVisibleMethodsByName(const DeclContext *DC,
-                                                DeclarationName Name) {
-    return false;
-  }
-
   /// Ensures that the table of all visible declarations inside this
   /// context is up to date.
   ///
