3e558af7f45ae98481a3b7299d688e62760efd65
Dave Lee <davelee.com@gmail.com>
2025-05-14 15:52:27 -0700

    Add tests

---
+++ lldb/test/API/lang/swift/async/formatters/task/name/TestSwiftTaskName.py
@@ -0,0 +1,24 @@
+import textwrap
+import lldb
+from lldbsuite.test.decorators import *
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+
+
+class TestCase(TestBase):
+
+    @swiftTest
+    def test_summary_contains_name(self):
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "break outside", lldb.SBFileSpec("main.swift")
+        )
+        self.expect("v task", patterns=[r'"Chore" id:[1-9]\d*'])
+
+    @swiftTest
+    def test_thread_contains_name(self):
+        self.build()
+        lldbutil.run_to_source_breakpoint(
+            self, "break inside", lldb.SBFileSpec("main.swift")
+        )
+        self.expect("thread info", patterns=[r"Chore \(Task [1-9]\d*\)"])
