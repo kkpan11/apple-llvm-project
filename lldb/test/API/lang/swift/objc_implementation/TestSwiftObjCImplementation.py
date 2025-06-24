1fe5cc8d5d6446ed29869ec4562e6804981df2ba
Dave Lee <davelee.com@gmail.com>
2025-06-23 19:02:51 -0700

    Fix test regex

--- lldb/test/API/lang/swift/objc_implementation/TestSwiftObjCImplementation.py
+++ lldb/test/API/lang/swift/objc_implementation/TestSwiftObjCImplementation.py
@@ -22,7 +22,7 @@ class TestCase(TestBase):
             ],
             # On x86_64, BOOL types have an objc encoding of 'c', which is a
             # signed char. The result is in an output of '\x01'.
-            patterns=[r"boolean = (true|'\x01')"],
+            patterns=[r"boolean = (true|'\\x01')"],
         )
         # Swift types that are not representable in ObjC (bridged types such as
         # String) are not currently listed in the children. rdar://154046212
