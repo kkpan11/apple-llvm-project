0a18d9a09a79cbc5e349aeb4379628d1ea079482
Yeoul Na <yeoul_na@apple.com>
2025-07-09 12:26:31 -0700

    [BoundsSafety][NFC] Don't emit wide pointer indexing in argument passing in order to avoid non-determinism
    The execution order of arguments is unspecified in C++. Directly
    passing calls that emit code as arguments caused non-deterministic
    order in the generated instructions and test failures in different
    platforms.

--- clang/lib/CodeGen/CGExprAgg.cpp
+++ clang/lib/CodeGen/CGExprAgg.cpp
@@ -441,13 +441,13 @@ void AggExprEmitter::EmitWidePointerBitCast(CastExpr *E) {
   RValue SrcRV = CGF.EmitAnyExpr(E->getSubExpr());
   assert(SrcRV.isAggregate());
   Address SrcAddr = SrcRV.getAggregateAddress();
-  EmitWidePointerToDest(E->getType(),
-                        CGF.GetWidePointerElement(SrcAddr, WPIndex::Pointer),
-                        CGF.GetWidePointerElement(SrcAddr, WPIndex::Upper),
-                        E->getType()->isBidiIndexablePointerType()
+  llvm::Value *Ptr = CGF.GetWidePointerElement(SrcAddr, WPIndex::Pointer);
+  llvm::Value *Upper = CGF.GetWidePointerElement(SrcAddr, WPIndex::Upper);
+  llvm::Value *Lower = E->getType()->isBidiIndexablePointerType()
                             ? CGF.GetWidePointerElement(SrcAddr, WPIndex::Lower)
-                            : nullptr,
-                        ElemBitCast);
+                            : nullptr;
+
+  EmitWidePointerToDest(E->getType(), Ptr, Upper, Lower, ElemBitCast);
 }
 
 void AggExprEmitter::EmitWidePointer(LValue DestLV, llvm::Value *Ptr,
