6933689dd7ecc00427d33ddd2dfdf10d7d081756
Henrik G. Olsson <hnrklssn@gmail.com>
2025-09-11 12:58:59 -0700

    [Utils] Add support for split-file to diff_test_updater (#157765)
    (cherry picked from commit 9eb17cc0343d09264ea875038901d1c6541dcef7)

--- llvm/utils/lit/lit/TestRunner.py
+++ llvm/utils/lit/lit/TestRunner.py
@@ -1175,7 +1175,7 @@ def executeScriptInternal(
         ):
             for test_updater in litConfig.test_updaters:
                 try:
-                    update_output = test_updater(result, test)
+                    update_output = test_updater(result, test, commands)
                 except Exception as e:
                     output = out
                     output += err
