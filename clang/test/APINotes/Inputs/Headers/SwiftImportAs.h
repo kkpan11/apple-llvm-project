f7313f7aa6b59bba72d2ef574355673646aeae73
Gábor Horváth <xazax.hun@gmail.com>
2025-09-15 12:30:29 +0200

    [APINotes] Support annotating safety of APIs (#157506)

--- clang/test/APINotes/Inputs/Headers/SwiftImportAs.h
+++ clang/test/APINotes/Inputs/Headers/SwiftImportAs.h
@@ -32,3 +32,14 @@ struct NoncopyableWithDestroyType {
 };
 
 void NCDDestroy(NoncopyableWithDestroyType instance);
+
+void ImportAsUnsafe();
+struct ImportAsUnsafeStruct {
+};
+struct StructWithUnsafeMethod {
+    void ImportAsUnsafeMethod();
+    void ImportAsUnsafeMethodActuallySafe();
+};
+
+void ImportAsUnsafeAlreadyAnnotated() __attribute__((swift_attr("unsafe")));
+void ImportAsUnsafeVersioned();
