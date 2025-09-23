0f5f40b023ce67e403a88c44f5862be9766354b4
Ebuka Ezike <yerimyah1@gmail.com>
2025-09-23 14:48:34 +0100

    [lldb][test] enable `swiftEmbeddedFrameVariable`
    Did not notice any failures, most likely fixed by another commit.

--- lldb/test/API/lang/swift/embedded/frame_variable/TestSwiftEmbeddedFrameVariable.py
+++ lldb/test/API/lang/swift/embedded/frame_variable/TestSwiftEmbeddedFrameVariable.py
@@ -3,7 +3,6 @@ from lldbsuite.test.lldbtest import *
 from lldbsuite.test.decorators import *
 import lldbsuite.test.lldbutil as lldbutil
 
-@skipIf(bugnumber = "rdar://159531308")
 class TestSwiftEmbeddedFrameVariable(TestBase):
     @skipUnlessDarwin
     @swiftTest
