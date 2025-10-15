fa583f2b4468285b08a09a7e3dbd154d7ba76710
Jakub Kuderski <jakub@nod-labs.com>
2025-10-15 09:27:37 -0400

    [ADT] Migrate StringSwitch Cases with 6+ arguments to new overload. NFC. (#163549)
    Switch to the `.Cases({S0, S1, ...}, Value)` overload instead, and the
    manually-enumerated overloads with 6+ arguments are getting deprecated
    in https://github.com/llvm/llvm-project/pull/163405.
    
    This pre-commits API updates ahead of the deprecation to make potential
    reverts cleaner. This was already reviewed in #163405.
    
    (cherry picked from commit 2ed7baafc3bf5b4321e3f77dfb1ce291e62bcadc)

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -4081,15 +4081,16 @@ static FormatAttrKind getFormatAttrKind(StringRef Format) {
       .Case("CFString", CFStringFormat)
       .Case("strftime", StrftimeFormat)
 
-      // Otherwise, check for supported formats.
-      .Cases("scanf", "printf", "printf0", "strfmon", SupportedFormat)
-      .Cases("cmn_err", "vcmn_err", "zcmn_err", SupportedFormat)
-      .Cases("kprintf", "syslog", SupportedFormat) // OpenBSD.
+  // Otherwise, check for supported formats.
+      .Cases({"scanf", "printf", "printf0", "strfmon"}, SupportedFormat)
+      .Cases({"cmn_err", "vcmn_err", "zcmn_err"}, SupportedFormat)
+      .Cases({"kprintf", "syslog"}, SupportedFormat) // OpenBSD.
       .Case("freebsd_kprintf", SupportedFormat)    // FreeBSD.
       .Case("os_trace", SupportedFormat)
       .Case("os_log", SupportedFormat)
 
-      .Cases("gcc_diag", "gcc_cdiag", "gcc_cxxdiag", "gcc_tdiag", IgnoredFormat)
+      .Cases({"gcc_diag", "gcc_cdiag", "gcc_cxxdiag", "gcc_tdiag"},
+             IgnoredFormat)
       .Default(InvalidFormat);
 }
 
