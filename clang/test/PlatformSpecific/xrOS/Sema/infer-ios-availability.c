d0a7b749add9c0f2beb0f1b466c869094752d0b8
Akira Hatanaka <ahatanaka@apple.com>
2025-11-19 18:00:00 -0800

    Check platforms and version numbers in the test

--- clang/test/PlatformSpecific/xrOS/Sema/infer-ios-availability.c
+++ clang/test/PlatformSpecific/xrOS/Sema/infer-ios-availability.c
@@ -12,8 +12,8 @@ __attribute__((availability(ios_app_extension, unavailable)))
 void ios_ext_unavail(); // ext-note {{}}
 
 void use() {
-  ios_unavail(); // ios-error {{'ios_unavail' is unavailable: not available on }}
-  ios_ext_unavail(); // ext-error {{'ios_ext_unavail' is unavailable: not available on }}
+  ios_unavail(); // ios-error {{'ios_unavail' is unavailable: not available on visionOS}}
+  ios_ext_unavail(); // ext-error {{'ios_ext_unavail' is unavailable: not available on visionOS}}
 }
 
 __attribute__((availability(ios, introduced=10)))
@@ -42,8 +42,8 @@ void useIntroduced() {
   ios_introduced_15();
   ios_ext_introduced_15();
   // introduced iOS 16 => xros unavailable (no mapping)
-  ios_introduced_16(); // ios-error {{is unavailable: not available on }}
-  ios_ext_introduced_16(); // ext-error {{is unavailable: not available on }}
+  ios_introduced_16(); // ios-error {{is unavailable: not available on visionOS}}
+  ios_ext_introduced_16(); // ext-error {{is unavailable: not available on visionOS}}
 }
 
 __attribute__((availability(ios, deprecated=10)))
@@ -69,15 +69,15 @@ void ios_ext_deprecated_16();
 
 void useDeprecated() {
   // deprecated iOS < 10 => deprecated xrOS 1
-  ios_deprecated_10(); // ios-warning {{is deprecated: first deprecated in}}
-  ios_ext_deprecated_10(); // ext-warning {{is deprecated: first deprecated in}}
+  ios_deprecated_10(); // ios-warning {{is deprecated: first deprecated in visionOS 1.0}}
+  ios_ext_deprecated_10(); // ext-warning {{is deprecated: first deprecated in visionOS (App Extension) 1.0}}
   // deprecated iOS 15 => deprecated xrOS 1
-  ios_deprecated_15(); // ios-warning {{is deprecated: first deprecated in}}
-  ios_ext_deprecated_15(); // ext-warning {{is deprecated: first deprecated in}}
+  ios_deprecated_15(); // ios-warning {{is deprecated: first deprecated in visionOS 1.0}}
+  ios_ext_deprecated_15(); // ext-warning {{is deprecated: first deprecated in visionOS (App Extension) 1.0}}
   // deprecated iOS 16 => deprecated xrOS 1.0.99
   ios_deprecated_16();
 #ifdef XROS2
-  // ios-warning@-2 {{is deprecated: first deprecated in}}
+  // ios-warning@-2 {{is deprecated: first deprecated in visionOS 1.0.99}}
 #endif
   ios_ext_deprecated_16();
 }
@@ -105,15 +105,15 @@ void ios_ext_obsoleted_16();
 
 void useObsoleted() {
   // deprecated iOS < 10 => deprecated xrOS 1
-  ios_obsoleted_10(); // ios-error {{is unavailable: obsoleted in}}
-  ios_ext_obsoleted_10(); // ext-error {{is unavailable: obsoleted in}}
+  ios_obsoleted_10(); // ios-error {{is unavailable: obsoleted in visionOS 1.0}}
+  ios_ext_obsoleted_10(); // ext-error {{is unavailable: obsoleted in visionOS (App Extension) 1.0}}
   // deprecated iOS 15 => deprecated xrOS 1
-  ios_obsoleted_15(); // ios-error {{is unavailable: obsoleted in}}
-  ios_ext_obsoleted_15(); // ext-error {{is unavailable: obsoleted in}}
+  ios_obsoleted_15(); // ios-error {{is unavailable: obsoleted in visionOS 1.0}}
+  ios_ext_obsoleted_15(); // ext-error {{is unavailable: obsoleted in visionOS (App Extension) 1.0}}
   // obsoleted iOS 16 => obsoleted xrOS 1.0.99
   ios_obsoleted_16();
 #ifdef XROS2
-  // ios-error@-2 {{is unavailable: obsoleted in}}
+  // ios-error@-2 {{is unavailable: obsoleted in visionOS 1.0.99}}
 #endif
   ios_ext_obsoleted_16();
 }
