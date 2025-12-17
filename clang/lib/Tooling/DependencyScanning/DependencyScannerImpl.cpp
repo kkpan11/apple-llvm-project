95f05620d4c16608655b36af74ac65d68859c7cc
Jan Svoboda <jan_svoboda@apple.com>
2025-12-17 12:18:28 -0800

    [clang] Remove pointless `hasDiagnostics()` checks (#172705)
    Calling `CompilerInstance::hasDiagnostics()` after
    `CompilerInstance::createDiagnostics()` is pointless, since the creation
    step cannot fail. This removes such calls.
    
    (cherry picked from commit db1fd3fd2a0c8a43acf543ac6dbe0f1d0c470430)

--- clang/lib/Tooling/DependencyScanning/DependencyScannerImpl.cpp
+++ clang/lib/Tooling/DependencyScanning/DependencyScannerImpl.cpp
@@ -615,7 +615,7 @@ initVFSForByNameScanning(IntrusiveRefCntPtr<llvm::vfs::FileSystem> BaseFS,
   return std::make_pair(OverlayFS, ModifiedCommandLine);
 }
 
-bool initializeScanCompilerInstance(
+void initializeScanCompilerInstance(
     CompilerInstance &ScanInstance,
     IntrusiveRefCntPtr<llvm::vfs::FileSystem> FS,
     DiagnosticConsumer *DiagConsumer, DependencyScanningService &Service,
@@ -630,8 +630,6 @@ bool initializeScanCompilerInstance(
     sanitizeDiagOpts(ScanInstance.getDiagnosticOpts());
 
   ScanInstance.createDiagnostics(DiagConsumer, /*ShouldOwnClient=*/false);
-  if (!ScanInstance.hasDiagnostics())
-    return false;
   if (VerboseOS)
     ScanInstance.setVerboseOutputStream(*VerboseOS);
 
@@ -687,8 +685,6 @@ bool initializeScanCompilerInstance(
 
   // Avoid some checks and module map parsing when loading PCM files.
   ScanInstance.getPreprocessorOpts().ModulesCheckRelocated = false;
-
-  return true;
 }
 
 llvm::SmallVector<StringRef>
@@ -844,10 +840,8 @@ bool DependencyScanningAction::runInvocation(
   ScanInstance.getInvocation().getCASOpts() = CASOpts;
 
   assert(!DiagConsumerFinished && "attempt to reuse finished consumer");
-  if (!initializeScanCompilerInstance(ScanInstance, FS, DiagConsumer, Service,
-                                      DepFS, DiagGenerationAsCompilation,
-                                      VerboseOS))
-    return false;
+  initializeScanCompilerInstance(ScanInstance, FS, DiagConsumer, Service, DepFS,
+                                 DiagGenerationAsCompilation, VerboseOS);
 
   llvm::SmallVector<StringRef> StableDirs = getInitialStableDirs(ScanInstance);
   auto MaybePrebuiltModulesASTMap =
@@ -971,10 +965,9 @@ bool CompilerInstanceWithContext::initialize(DiagnosticConsumer *DC) {
   auto &CI = *CIPtr;
 
   CI.getInvocation().getCASOpts() = Worker.CASOpts;
-  if (!initializeScanCompilerInstance(
-          CI, OverlayFS, DiagEngineWithCmdAndOpts->DiagEngine->getClient(),
-          Worker.Service, Worker.DepFS, false, nullptr))
-    return false;
+  initializeScanCompilerInstance(
+      CI, OverlayFS, DiagEngineWithCmdAndOpts->DiagEngine->getClient(),
+      Worker.Service, Worker.DepFS, false, nullptr);
 
   StableDirs = getInitialStableDirs(CI);
   auto MaybePrebuiltModulesASTMap =
