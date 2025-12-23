llvm/llvm-project

[-Wunsafe-buffer-usage] Do not warn about class methods with libc function names (#151270)
This commit fixes the false positive that C++ class methods with libc
function names would be false warned about. For example,

```
struct T {void strcpy() const;};
void test(const T& t) {  str.strcpy(); // no warn }
```

rdar://156264388

clang/lib/Analysis/UnsafeBufferUsage.cpp
@@ -1986,6 +1986,14 @@ public:
     const auto *FD = dyn_cast<FunctionDecl>(CE->getDirectCallee());
     if (!FD)
       return false;
+
+    bool IsGlobalAndNotInAnyNamespace =
+        FD->isGlobal() && !FD->getEnclosingNamespaceContext()->isNamespace();
+
+    // A libc function must either be in the std:: namespace or a global
+    // function that is not in any namespace:
+    if (!FD->isInStdNamespace() && !IsGlobalAndNotInAnyNamespace)
+      return false;
     auto isSingleStringLiteralArg = false;
     if (CE->getNumArgs() == 1) {
       isSingleStringLiteralArg =

kkpan11/apple-llvm-project

(cherry picked from commit 0abf4975bbf176d393869d290d55748794e220c4)
 Conflicts:
	clang/lib/Analysis/UnsafeBufferUsage.cpp

clang/lib/Analysis/UnsafeBufferUsage.cpp
@@ -2984,6 +2984,15 @@ public:
 
     if (!CalleeDecl)
       return false;
+
+    bool IsGlobalAndNotInAnyNamespace =
+        CalleeDecl->isGlobal() &&
+        !CalleeDecl->getEnclosingNamespaceContext()->isNamespace();
+
+    // A libc function must either be in the std:: namespace or a global
+    // function that is not in any namespace:
+    if (!CalleeDecl->isInStdNamespace() && !IsGlobalAndNotInAnyNamespace)
+      return false;
     // If the call has a sole null-terminated argument, e.g., strlen,
     //  printf, atoi, we consider it safe:
     if (hasNumArgs(Call, 1) && isNullTermPointer(Call->getArg(0), Ctx))