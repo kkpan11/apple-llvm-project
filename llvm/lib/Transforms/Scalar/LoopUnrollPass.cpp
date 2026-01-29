08161ab77db83e37ee3f3bc1cf823d822d91225b
Guy David <guyda@apple.com>
2026-01-29 09:59:16 +0200

    [LoopPeel] Peel last iteration to enable load widening
    This change adds support for peeling the last iteration of a loop
    to enable consecutive load widening. When multiple small loads
    (e.g., 3 i8 loads) span an odd number of bytes, they can be combined
    into a single wider load (e.g., 1 i32 load) after peeling the last
    iteration doubles the dereferenceable bytes.

--- llvm/lib/Transforms/Scalar/LoopUnrollPass.cpp
+++ llvm/lib/Transforms/Scalar/LoopUnrollPass.cpp
@@ -920,14 +920,17 @@ shouldPartialUnroll(const unsigned LoopSize, const unsigned TripCount,
 // FIXME: This function is used by LoopUnroll and LoopUnrollAndJam, but consumes
 // many LoopUnroll-specific options. The shared functionality should be
 // refactored into it own function.
-bool llvm::computeUnrollCount(
-    Loop *L, const TargetTransformInfo &TTI, DominatorTree &DT, LoopInfo *LI,
-    AssumptionCache *AC, ScalarEvolution &SE,
-    const SmallPtrSetImpl<const Value *> &EphValues,
-    OptimizationRemarkEmitter *ORE, unsigned TripCount, unsigned MaxTripCount,
-    bool MaxOrZero, unsigned TripMultiple, const UnrollCostEstimator &UCE,
-    TargetTransformInfo::UnrollingPreferences &UP,
-    TargetTransformInfo::PeelingPreferences &PP, bool &UseUpperBound) {
+bool llvm::computeUnrollCount(Loop *L, const TargetTransformInfo &TTI,
+                              DominatorTree &DT, LoopInfo *LI,
+                              AssumptionCache *AC, ScalarEvolution &SE,
+                              const SmallPtrSetImpl<const Value *> &EphValues,
+                              OptimizationRemarkEmitter *ORE,
+                              unsigned TripCount, unsigned MaxTripCount,
+                              bool MaxOrZero, unsigned TripMultiple,
+                              const UnrollCostEstimator &UCE,
+                              TargetTransformInfo::UnrollingPreferences &UP,
+                              TargetTransformInfo::PeelingPreferences &PP,
+                              bool &UseUpperBound, bool AllowLoadWideningPeel) {
 
   unsigned LoopSize = UCE.getRolledLoopSize();
 
@@ -1013,7 +1016,8 @@ bool llvm::computeUnrollCount(
   }
 
   // 5th priority is loop peeling.
-  computePeelCount(L, LoopSize, PP, TripCount, DT, SE, TTI, AC, UP.Threshold);
+  computePeelCount(L, LoopSize, PP, TripCount, DT, SE, TTI, AC, UP.Threshold,
+                   AllowLoadWideningPeel);
   if (PP.PeelCount) {
     UP.Runtime = false;
     UP.Count = 1;
@@ -1292,10 +1296,14 @@ tryToUnrollLoop(Loop *L, DominatorTree &DT, LoopInfo *LI, ScalarEvolution &SE,
 
   // computeUnrollCount() decides whether it is beneficial to use upper bound to
   // fully unroll the loop.
+  // When OnlyFullUnroll is true, we're running before vectorization
+  // (LoopFullUnrollPass), so disable load widening peeling to avoid peeling
+  // loops that could have been vectorized instead.
   bool UseUpperBound = false;
   bool IsCountSetExplicitly = computeUnrollCount(
       L, TTI, DT, LI, &AC, SE, EphValues, &ORE, TripCount, MaxTripCount,
-      MaxOrZero, TripMultiple, UCE, UP, PP, UseUpperBound);
+      MaxOrZero, TripMultiple, UCE, UP, PP, UseUpperBound,
+      /*AllowLoadWideningPeel=*/!OnlyFullUnroll);
   if (!UP.Count)
     return LoopUnrollResult::Unmodified;
 
@@ -1313,16 +1321,17 @@ tryToUnrollLoop(Loop *L, DominatorTree &DT, LoopInfo *LI, ScalarEvolution &SE,
     });
 
     ValueToValueMapTy VMap;
-    if (peelLoop(L, PP.PeelCount, PP.PeelLast, LI, &SE, DT, &AC, PreserveLCSSA,
-                 VMap)) {
-      simplifyLoopAfterUnroll(L, true, LI, &SE, &DT, &AC, &TTI, nullptr);
-      // If the loop was peeled, we already "used up" the profile information
-      // we had, so we don't want to unroll or peel again.
-      if (PP.PeelProfiledIterations)
-        L->setLoopAlreadyUnrolled();
-      return LoopUnrollResult::PartiallyUnrolled;
-    }
-    return LoopUnrollResult::Unmodified;
+    peelLoop(L, PP.PeelCount, PP.PeelLast, LI, &SE, DT, &AC, PreserveLCSSA,
+             VMap);
+    // Widen consecutive loads after last-iteration peeling
+    if (PP.PeelLast)
+      widenLoadsAfterPeel(*L, SE, TTI, DT);
+    simplifyLoopAfterUnroll(L, true, LI, &SE, &DT, &AC, &TTI, nullptr);
+    // If the loop was peeled, we already "used up" the profile information
+    // we had, so we don't want to unroll or peel again.
+    if (PP.PeelProfiledIterations)
+      L->setLoopAlreadyUnrolled();
+    return LoopUnrollResult::PartiallyUnrolled;
   }
 
   // Do not attempt partial/runtime unrolling in FullLoopUnrolling
