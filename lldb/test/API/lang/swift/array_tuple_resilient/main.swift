16c004bb182f8f333305b431cd8642b8b10edcd4
Dave Lee <davelee.com@gmail.com>
2026-03-16 17:39:38 -0700

    [lldb] Update url format in TestSwiftArrayTupleResilient.py

--- lldb/test/API/lang/swift/array_tuple_resilient/main.swift
+++ lldb/test/API/lang/swift/array_tuple_resilient/main.swift
@@ -7,11 +7,11 @@ var patatino : [(URL, Int64)] = [(URL(string: "https://github.com")!, 1001)]
 var tinky : [(URL, URL)] = [(URL(string: "https://github.com")!,
                             URL(string: "https://github.com")!)]
 print(patatino) //%self.expect('frame variable -d run -- patatino',
-                //%             substrs=['[0] = (0 = "https://github.com", 1 = 1001)'])
+                //%             substrs=['[0] = (0 = https://github.com, 1 = 1001)'])
                 //%self.expect('expr -d run -- patatino',
-                //%             substrs=['[0] = (0 = "https://github.com", 1 = 1001)'])
+                //%             substrs=['[0] = (0 = https://github.com, 1 = 1001)'])
 
 print(tinky)    //%self.expect('frame variable -d run -- tinky',
-                //%             substrs=['[0] = (0 = "https://github.com", 1 = "https://github.com")'])
+                //%             substrs=['[0] = (0 = https://github.com, 1 = https://github.com)'])
                 //%self.expect('expr -d run -- tinky',
-                //%             substrs=['[0] = (0 = "https://github.com", 1 = "https://github.com")'])
+                //%             substrs=['[0] = (0 = https://github.com, 1 = https://github.com)'])
