2f991e2ead2c13400050dd3b57a6f566d64f3337
Charles Zablit <c_zablit@apple.com>
2026-06-13 10:47:30 +0200

    Revert "[lldb] Enable Swift Foundation value-type formatters on Windows"
    This reverts commit ffb559b3f2a0a52fe49d32380887b4927e77c4e9.

--- lldb/test/API/lang/swift/foundation_value_types/date/TestSwiftFoundationTypeDate.py
+++ lldb/test/API/lang/swift/foundation_value_types/date/TestSwiftFoundationTypeDate.py
@@ -14,4 +14,4 @@ from lldbsuite.test.decorators import *
 
 lldbinline.MakeInlineTest(__file__, globals(),
                           decorators=[skipEmbeddedSwift,
-        swiftTest,skipIf(oslist=["linux"])])
+        swiftTest,skipUnlessFoundation,skipIf(oslist=['windows'])])
