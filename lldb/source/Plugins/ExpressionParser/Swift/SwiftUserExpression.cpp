08e8a27e774f381fd95659dd1f3d9d54c4273d0b
Dave Lee <davelee.com@gmail.com>
2026-06-16 20:20:42 -0700

    Group assignments for context-free

--- lldb/source/Plugins/ExpressionParser/Swift/SwiftUserExpression.cpp
+++ lldb/source/Plugins/ExpressionParser/Swift/SwiftUserExpression.cpp
@@ -303,10 +303,13 @@ bool SwiftUserExpression::ScanContext(DiagnosticManager &diagnostic_manager,
       m_is_weak_self = true;
     }
 
-  if (m_options.GetUseContextFreeSwiftPrintObject())
+  if (m_options.GetUseContextFreeSwiftPrintObject()) {
+    // Prevent need for extensions, or passing context, in WrapExpression.
     m_in_static_method = false;
-  m_needs_object_ptr =
-      !m_in_static_method && !m_options.GetUseContextFreeSwiftPrintObject();
+    m_needs_object_ptr = false;
+  } else {
+    m_needs_object_ptr = !m_in_static_method;
+  }
   LLDB_LOGF(log, "  [SUE::SC] Expression captures self: %s",
             m_needs_object_ptr ? "true" : "false");
 
