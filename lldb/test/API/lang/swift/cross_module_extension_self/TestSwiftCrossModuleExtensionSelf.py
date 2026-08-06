93aa5b3885828446cf072af83f452bf576acc2f0
Adrian Prantl <aprantl@apple.com>
2026-08-06 13:51:00 -0700

    work around missing decorator on 6.4.x

--- lldb/test/API/lang/swift/cross_module_extension_self/TestSwiftCrossModuleExtensionSelf.py
+++ lldb/test/API/lang/swift/cross_module_extension_self/TestSwiftCrossModuleExtensionSelf.py
@@ -12,7 +12,7 @@ import lldbsuite.test.lldbutil as lldbutil
 
 
 class TestSwiftCrossModuleExtensionSelf(TestBase):
-    @requireNotEmbeddedSwift
+    @skipEmbeddedSwift # 6.4.x only
     @swiftTest
     def test_unusable_self_does_not_break_frame(self):
         self.build()
