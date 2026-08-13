37b2dd3b9ca1279101d759e8985744b089325c9a
Augusto Noronha <anoronha@apple.com>
2026-08-13 15:29:51 -0700

    [lldb] Don't substitute Clang mapped types with missing overlays in embedded Swift (#13762) (#13836)
    TypeSystemSwiftTypeRef textually includes the compiler's
    lib/ClangImporter/MappedTypes.def and applied every row unconditionally, with no
    mangling-flavor check. Embedded Swift doesn't have overlays for types
    that are not on the swift standard library (for example, CGFloat), so
    this failed to produce the correct type name.
    
    rdar://184659844
    
    (cherry picked from commit 97b37025287a42bef435c3e890bb42d88ffcd647)

--- lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntimeDynamicTypeResolution.cpp
+++ lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntimeDynamicTypeResolution.cpp
@@ -280,11 +280,14 @@ MemoryReaderLocalBufferHolder SwiftLanguageRuntime::PushLocalBuffer(uint64_t loc
 class LLDBTypeInfoProvider : public swift::remote::TypeInfoProvider {
   SwiftLanguageRuntime &m_runtime;
   TypeSystemSwiftTypeRef &m_ts;
+  /// The mangling flavor of the program the type info is being read for.
+  swift::Mangle::ManglingFlavor m_flavor;
 
 public:
   LLDBTypeInfoProvider(SwiftLanguageRuntime &runtime,
-                       TypeSystemSwiftTypeRef &ts)
-      : m_runtime(runtime), m_ts(ts) {}
+                       TypeSystemSwiftTypeRef &ts,
+                       swift::Mangle::ManglingFlavor flavor)
+      : m_runtime(runtime), m_ts(ts), m_flavor(flavor) {}
 
   swift::remote::TypeInfoProvider::IdType getId() override {
     return (void *)((char *)&m_ts + m_runtime.GetGeneration());
@@ -427,9 +430,8 @@ public:
           auto *key = swift_type.GetMangledTypeName().AsCString(nullptr);
           m_runtime.RegisterAnonymousClangType(key, field_type);
         } else {
-          // TODO: The mangling flavor should be threaded through getTypeInfo.
-          swift_type = typesystem.ConvertClangTypeToSwiftType(
-              field_type, swift::Mangle::ManglingFlavor::Default);
+          swift_type =
+              typesystem.ConvertClangTypeToSwiftType(field_type, m_flavor);
         }
         const swift::reflection::TypeRef *typeref = nullptr;
         auto typeref_or_err = m_runtime.GetTypeRef(swift_type, &typesystem);
@@ -1060,7 +1062,7 @@ SwiftRuntimeTypeVisitor::VisitImpl(std::optional<unsigned> visit_only,
       if (instance != LLDB_INVALID_ADDRESS) {
         auto *desc_finder =
             ts.GetDescriptorFinder(m_exe_ctx.GetBestExecutionContextScope());
-        LLDBTypeInfoProvider tip(m_runtime, ts);
+        LLDBTypeInfoProvider tip(m_runtime, ts, m_flavor);
         ThreadSafeReflectionContext reflection_ctx =
             m_runtime.GetReflectionContext();
         if (reflection_ctx) {
@@ -1318,7 +1320,7 @@ SwiftRuntimeTypeVisitor::VisitImpl(std::optional<unsigned> visit_only,
               llvm::dyn_cast_or_null<swift::reflection::ObjCClassTypeRef>(tr))
         return visit_objcclass(*obj_tr);
 
-      LLDBTypeInfoProvider tip(m_runtime, ts);
+      LLDBTypeInfoProvider tip(m_runtime, ts, m_flavor);
       auto cti_or_err = reflection_ctx->GetClassInstanceTypeInfo(
           *tr, &tip,
           ts.GetDescriptorFinder(m_exe_ctx.GetBestExecutionContextScope()));
@@ -1441,7 +1443,7 @@ SwiftRuntimeTypeVisitor::VisitImpl(std::optional<unsigned> visit_only,
       return supers.size() >= 2;
     };
 
-    LLDBTypeInfoProvider tip(m_runtime, ts);
+    LLDBTypeInfoProvider tip(m_runtime, ts, m_flavor);
     lldb::addr_t instance = ::MaskMaybeBridgedPointer(
         m_runtime.GetProcess(), m_valobj->GetPointerValue().address);
 
@@ -1771,7 +1773,7 @@ SwiftLanguageRuntime::ProjectEnum(ValueObject &valobj) {
 
     ThreadSafeReflectionContext reflection_ctx = GetReflectionContext();
     // The indirect enum field should point to a closure context.
-    LLDBTypeInfoProvider tip(*this, ts);
+    LLDBTypeInfoProvider tip(*this, ts, flavor);
     auto ti_or_err = reflection_ctx->GetTypeInfoFromInstance(
         pointer, &tip, ts.GetDescriptorFinder());
     if (!ti_or_err)
@@ -1987,7 +1989,8 @@ bool SwiftLanguageRuntime::ForEachSuperClassType(
   auto &ts = *tr_ts;
 
   ExecutionContext exe_ctx(instance.GetExecutionContextRef());
-  LLDBTypeInfoProvider tip(*this, ts);
+  auto flavor = GetManglingFlavor(instance_type.GetMangledTypeName());
+  LLDBTypeInfoProvider tip(*this, ts, flavor);
   lldb::addr_t pointer = instance.GetPointerValue().address;
   return reflection_ctx->ForEachSuperClassType(&tip, ts.GetDescriptorFinder(),
                                                pointer, fn);
@@ -4180,7 +4183,7 @@ SwiftLanguageRuntime::GetSwiftRuntimeTypeInfo(
       flavor == swift::Mangle::ManglingFlavor::Embedded ? g_debuginfo
                                                         : g_reflection,
       &GetProcess().GetTarget().GetDebugger(), *GetMemoryReader());
-  LLDBTypeInfoProvider provider(*this, ts);
+  LLDBTypeInfoProvider provider(*this, ts, flavor);
   return reflection_ctx->GetTypeInfo(type, &provider,
                                      ts.GetDescriptorFinder(exe_scope));
 }
