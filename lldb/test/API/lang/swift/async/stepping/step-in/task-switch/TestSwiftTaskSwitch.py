644398696352bb67d507ca21a35b7adeaf8818e8
Augusto Noronha <anoronha@apple.com>
2026-09-09 16:29:56 -0700

    [rebranch][lldb] Re-skip 3 tests on embedded mode
    00570ca47b enabled them, but there's something different on rebranch
    that makes them fail

--- lldb/test/API/lang/swift/async/stepping/step-in/task-switch/TestSwiftTaskSwitch.py
+++ lldb/test/API/lang/swift/async/stepping/step-in/task-switch/TestSwiftTaskSwitch.py
@@ -5,6 +5,7 @@ import lldbsuite.test.lldbutil as lldbutil
 
 
 class TestCase(lldbtest.TestBase):
+    @skipEmbeddedSwift
     @swiftTest
     @skipIf(oslist=["windows", "linux"])
     def test(self):
