6887c00bf20491b5c51a9db43dc6614defae4067
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2026-09-15 12:38:16 +0100

    [lldb][NFC] Fix position of comment
    (cherry picked from commit 465010fd2c6a41891c30f25763650165065d97f9)

--- lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntime.cpp
+++ lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntime.cpp
@@ -2493,9 +2493,6 @@ static void PrintFramesForTask(ExecutionContext &exe_ctx,
   }
 }
 
-/// A helper class to find Tasks in the swift program. It implements the
-/// algorithm described in g_task_list_tree_common_text.
-
 static std::optional<std::vector<lldb::addr_t>>
 FindTaskAddrsFromRegistry(ReflectionContextInterface &reflection_ctx,
                           Process &process) {
@@ -2572,6 +2569,8 @@ FindTaskAddrs(ReflectionContextInterface &reflection_ctx, Process &process) {
   return FindTaskAddrsFromThreadList(process);
 }
 
+/// A helper class to find Tasks in the swift program. It implements the
+/// algorithm described in g_task_list_tree_common_text.
 class TaskExplorer {
 public:
   TaskExplorer(ReflectionContextInterface &reflection_ctx, Process &process)
