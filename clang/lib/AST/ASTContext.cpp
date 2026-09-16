e7d8c82a9c852e66a6aa96345d3ea160f176d5e4
Anthony Latsis <alatsis@apple.com>
2026-09-16 13:20:06 +0100

    Revert "[NFC][clang] AST: Reinstate renamed method to avoid swift rebranch change"
    This reverts commit b65a01f25df4cc5aa5b4944fe67cf14ad592aeec.

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -329,10 +329,6 @@ RawComment *ASTContext::getRawCommentNoCacheImpl(
   return CommentBeforeDecl;
 }
 
-RawComment *ASTContext::getRawCommentForDeclNoCache(const Decl *D) const {
-  return getRawCommentNoCache(D);
-}
-
 RawComment *ASTContext::getRawCommentNoCache(RawCommentLookupKey Key) const {
   const auto Locs = getLocsForCommentSearch(Key, SourceMgr);
 
