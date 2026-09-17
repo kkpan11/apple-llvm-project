b950607b1304e829fb48b72e89c33e4e2f40a5e6
Anthony Latsis <alatsis@apple.com>
2026-09-17 17:08:48 +0100

    Revert "[NFC] Reinstate several `DenseMapInfo::{getEmptyKey,getTombstoneKey}` to avoid swift rebranch change"
    This reverts commit 119276bc5be6616c3e4eed37e1331f1ed88ff21e.
    
    rdar://182730910

--- llvm/include/llvm/ADT/DenseMapInfo.h
+++ llvm/include/llvm/ADT/DenseMapInfo.h
@@ -63,15 +63,6 @@ template <typename T> struct DenseMapInfo<T *> {
   }
 
   static bool isEqual(const T *LHS, const T *RHS) { return LHS == RHS; }
-
-  static inline T *getEmptyKey() {
-    assert(false && "Obsoleted");
-    abort();
-  }
-  static inline T *getTombstoneKey() {
-    assert(false && "Obsoleted");
-    abort();
-  }
 };
 
 // Provide DenseMapInfo for all integral types.
@@ -86,15 +77,6 @@ struct DenseMapInfo<T, std::enable_if_t<std::is_integral_v<T>>> {
   }
 
   static bool isEqual(const T &LHS, const T &RHS) { return LHS == RHS; }
-
-  static inline T getEmptyKey() {
-    assert(false && "Obsoleted");
-    abort();
-  }
-  static inline T getTombstoneKey() {
-    assert(false && "Obsoleted");
-    abort();
-  }
 };
 
 // Provide DenseMapInfo for all pairs whose members have info.
--- llvm/include/llvm/IR/Attributes.h
+++ llvm/include/llvm/IR/Attributes.h
@@ -532,15 +532,6 @@ template <> struct DenseMapInfo<AttributeSet, void> {
   }
 
   static bool isEqual(AttributeSet LHS, AttributeSet RHS) { return LHS == RHS; }
-
-  static inline AttributeSet getEmptyKey() {
-    assert(false && "Obsoleted");
-    abort();
-  }
-  static inline AttributeSet getTombstoneKey() {
-    assert(false && "Obsoleted");
-    abort();
-  }
 };
 
 //===----------------------------------------------------------------------===//
