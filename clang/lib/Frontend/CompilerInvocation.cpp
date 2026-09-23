4a3428c475db014078a0c9582a7d7b5ed63255c4
Oliver Hunt <oliver@apple.com>
2026-09-23 16:23:54 -0700

    [clang][tmo] Remove GPU driver compatibility error (#14355)
    This restriction was just an overly paranoid change. I thought
    I had identified a codegen bug, but it was actually purely a
    Sema error, and attributes like `cleanup` have the same error
    mode.

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -5612,20 +5612,6 @@ bool CompilerInvocation::CreateFromArgsImpl(
     if (LangOpts.CUDAIsDevice)
       Res.getTargetOpts().HostTriple = Res.getFrontendOpts().AuxTriple;
   }
-  auto InvalidTMOLanguageMode = [&LangOpts]() -> const char * {
-    if (!LangOpts.TypedMemoryOperations)
-      return nullptr;
-    if (LangOpts.OpenCL)
-      return "OpenCL";
-    if (LangOpts.HIP)
-      return "HIP";
-    if (LangOpts.CUDA)
-      return "CUDA";
-    return nullptr;
-  };
-  if (const char *InvalidMode = InvalidTMOLanguageMode())
-    Diags.Report(diag::err_drv_unsupported_opt_for_language_mode)
-        << "-ftyped-memory-operations" << InvalidMode;
 
   if (LangOpts.OpenACC && !Res.getFrontendOpts().UseClangIRPipeline &&
       isCodeGenAction(Res.getFrontendOpts().ProgramAction))
